d8ecab77cef52c7e38772e2e84fd4d1d4aebd758
Hector Lozano <hlozano2@cnm.edu>
2016-10-25 16:43:49 -0600

    added persona devil bill

---
+++ epic/devilbill.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+
+		<title>Devil Bill</title>
+	</head>
+	<body>
+		<section>
+				<h2>Persona</h2>
+				<p>
+					Name: Devil Bill
+				</p>
+				<p>
+					Age: 50
+				</p>
+
+				<p>
+					Profession: Bill is a hard core retired marine looking to stay in touch with all current or former Devil Doc's based on common duty stations.
+				</p>
+
+				<p>
+					Technology: Extremely technologically challenged, Was a marine who's career time was totally spent in an infantry unit. Served five back to back tours in Iraq and two more in Afghanistan. Owns a Motorola Razor flip phone and has no interest in ever owning a smart phone.
+				</p>
+				<p>
+					Attitude and Behaviors: Now retired, Bill has an outstanding attitude towards life, loves to fish and camp. Once a month he gets toghether with other retired members of the armed forces at Cabezon Wounded Warrior Haven for some real life war games. As Bill puts it: "You can take Bill out of the marines, but you will never take the marine out of Bill"
+				</p>
+				<p>
+					Frustrations and Needs:  Like a large percentage of military veterans, Bill suffers from PTSD and other injuries, too many to list.  Bill is a member of the local VFW and even though this has been the primary way he interacts with friends, he is sick and tired of the non-stop beer drinking. Bill wishes there was a better way to stay in touch with friends that served with him.
+				</p>
+				<p>
+					Goals:
+				</p>
+
+		</section>
+
