91b778d93b2ab76536533a7c3d60a2e6be2977d5
James Montoya <jamesmontoyaarts@gmail.com>
2016-10-27 16:30:02 -0600

    updated exec summary in index.php

--- epic/index.php
+++ epic/index.php
@@ -4,12 +4,16 @@
 		<meta charset="UTF-8">
 		<title>Title</title>
 	</head>
+
 	<body>
+
 		<h2>Executive Summary</h2>
-		<p>The Cannaduces application will be act as a medium to connect patients to local dispensaries and communinity support. Cannaduces is based on Albuquerque's large population of U.S. Veterians and its growing market for medical cannabis. Our main target market focuses are 0) U.S Veterians in need of medical cannabis. We want to bring all of the resoruces togehter in an effort to make them more accessible. 1) We also desire to be a directory source for people who are not native New Mexicans'. 2) Lastly we want to support a more communal environment between dispensaries while promoting positive awareness around the needs and benefits of medical cannabis. Our overall goal for Cannaduces is connection and accessibility. We want to put medical cannabis at your finger tips making it easy to find via GPS and helping user connect to the community of patients around them and all resources specific to thier needs.
+
+		<p>The Cannaduces application will act as the premier way to connect patients to local dispensaries and community cannabis support. Cannaduces is made to serve the growing population of cannabis users in Albuquerque.  Our main target market focuses are 0) U.S Veterians in need of medical cannabis; We want to bring all cannabis related resources in the state togehter in an effort to make them more accessible. 1) We also desire to be a directory source for people who are not native New Mexicans and patients new to the program. 2) Lastly we want to create a communal environment between dispensaries and patients while also promoting positive awareness around the needs of patients and the benefits of medical cannabis. Our overall goal for Cannaduces is connection and accessibility and information. We want to put medical cannabis at your finger tips making it easy to find the right medication via GPS, help users connect to the community of patients around them and all the resources specific to thier needs.
 		</p>
 
 		<h2>System Goals</h2>
+
 		<p> By using Cannaduceus users will have access to all information regarding medical cannabis at their fingertip. They will be able to gps locate all dispensaries in state, as well as find a wealth of information on the site/ app. Users will be able to review different dispensaries sharing thier expereince. Users will also have access to local medical cannabis events. There will be a section that keeps patients up to date with all local laws and legislature pretaining to medical cannabis. And lastly for the users over all experience there will be a section that will highlight differnt products avail such as edible and concentrated oils; as well as
 		</p>
 
