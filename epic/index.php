947cd8173f6a0cdd8189ec390e95941d7ac9fc5d
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-27 15:09:43 -0600

    executive summary

--- epic/index.php
+++ epic/index.php
@@ -6,8 +6,7 @@
 	</head>
 	<body>
 		<h2>Executive Summary</h2>
-		<p>
-
+		<p>The Cannaduces application will be act as a medium to connect patients to local dispensaries and communinity support. Cannaduces is based on Albuquerque's large population of U.S. Veterians and its growing market for medical cannabis. Our main target market focuses are 0) U.S Veterians in need of medical cannabis. We want to bring all of the resoruces togehter in an effort to make them more accessible. 1) We also desire to be a directory source for people who are not native New Mexicans'. 2) Lastly we want to support a more communal environment between dispensaries while promoting positive awareness around the needs and benefits of medical cannabis. Our overall goal for Cannaduces is connection and accessibility. We want to put medical cannabis at your finger tips making it easy to find via GPS and helping user connect to the community of patients around them and all resources specific to thier needs.
 		</p>
 
 		<h2>System Goals</h2>
