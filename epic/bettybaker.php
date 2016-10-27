15f4c94db3a5e1b6149c7fa14393d9ec87119ebc
Nathan Sanchez <nsanchez121@cnm.edu>
2016-10-27 12:49:22 -0600

    Betty Baker 2.0

--- epic/bettybaker.php
+++ epic/bettybaker.php
@@ -1,22 +1,34 @@
 <!DOCTYPE html>
 	<html lang="en">
 	<head>
+
 		<meta charset="UTF-8"/>
-	<title>Betty Baker</title>
+		<title>Betty Baker</title>
+
 	</head>
+
 	<h1>Betty Baker</h1>
+
 	<ol>
+
 		<li>Age: 47</li>
 		<li>Profession: Lawyer</li>
 		<li>Illment: Fibrumigula</li>
 		<li>Technology: iphone 7s plus, 2012 macbook pro, ipod</li>
 		<li>Attitude: Eastcoast attitude very straight forward blunt always in a rush. She hate for her time to wasted and she will accept nothing but professionalism . shes new to the area but not to medical marijuana, once she finds a dispensary she will be a loyal customer </li>
 		<li>Needs/Frustration: chronic pain being new to the area not knowing where to go</li>
-	</ol>
-<h2>Usecase</h2>
-	<p>
 
+	</ol>
+
+	<h2>Use Story</h2>
+
+	<p>
+		Betty Baker is looking for a app to show her where to go. To help her on her journy to get a medical card in New Mexico, and be able to see the dispensaries near her and what they have.
 	</p>
+
+	<h2>Use Case</h2>
+
+	<p>Betty is sitting in on a company meeting, when her chronic pain begains to act up. After the meeting she decides that shes tired of feeling so much pain she, pulls out her iphone7s and unlocks it navigates to the app store, taps on search and types medical marijuana New Mexico a list of apps pop up she sees the cannaduceus app, clicks on the app reads the reviews they all seem positve. So she downloads the app, when the app is done downloading she opens it up sees the navigation bar with medical card info, reviews, locations of dispensers, news, she taps on the medical card info tap and reads all the info regarding getting your medical card. Then she taps on the tab for locations types in her area code and sees that there is sevral dispensaries near her </p>
 
 
 
