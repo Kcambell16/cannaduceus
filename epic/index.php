38df02f8ffebe6114c9e6deab23f930117a48d77
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-28 09:31:51 -0600

    spell check changes

--- epic/index.php
+++ epic/index.php
@@ -10,12 +10,12 @@
 
 		<h2>Executive Summary</h2>
 
-		<p>The Cannaduces application will be act as a medium to connect patients to local dispensaries and communinity support. Cannaduces is based on Albuquerque's large population of U.S. Veterians and its growing market for medical cannabis. Our main target market focuses are 0) U.S Veterians in need of medical cannabis. We want to bring all of the resoruces togehter in an effort to make them more accessible. 1) We also desire to be a directory source for people who are not native New Mexicans'. 2) Lastly we want to support a more communal environment between dispensaries while promoting positive awareness around the needs and benefits of medical cannabis. Our overall goal for Cannaduces is connection and accessibility. We want to put medical cannabis at your finger tips making it easy to find via GPS and helping user connect to the community of patients around them and all resources specific to thier needs.
+		<p>The Cannaduces application will be act as a medium to connect patients to local dispensaries and community support. Cannaduces is based on Albuquerque's large population of U.S. Veterans and its growing market for medical cannabis. Our main target market focuses are 0) U.S Veterans in need of medical cannabis. We want to bring all of the resoruces together in an effort to make them more accessible. 1) We also desire to be a directory source for people who are not native New Mexicans'. 2) Lastly we want to support a more communal environment between dispensaries while promoting positive awareness around the needs and benefits of medical cannabis. Our overall goal for Cannaduces is connection and accessibility. We want to put medical cannabis at your finger tips making it easy to find via GPS and helping user connect to the community of patients around them and all resources specific to thier needs.
 		</p>
 
 		<h2>System Goals</h2>
 
-		<p> By using Cannaduceus users will have access to all information regarding medical cannabis at their fingertip. They will be able to gps locate all dispensaries in state, as well as find a wealth of information on the site/ app. Users will be able to review different dispensaries sharing thier expereince. Users will also have access to local medical cannabis events. There will be a section that keeps patients up to date with all local laws and legislature pretaining to medical cannabis. And lastly for the users over all experience there will be a section that will highlight differnt products avail such as edible and concentrated oils; as well as
+		<p> By using Cannaduceus users will have access to all information regarding medical cannabis at their fingertip. They will be able to gps locate all dispensaries in state, as well as find a wealth of information on the site/ app. Users will be able to review different dispensaries sharing their experience. Users will also have access to local medical cannabis events. There will be a section that keeps patients up to date with all local laws and legislature pertaining to medical cannabis. And lastly for the users over all experience there will be a section that will highlight different products avail such as edible and concentrated oils; as well as
 		</p>
 
 		<h1>Persona</h1>
@@ -24,7 +24,7 @@
 		<li>Age: 47</li>
 		<li>Profession: Lawyer</li>
 		<li>Technology: iphone 7s plus, 2012 macbook pro, ipod</li>
-		<li>Attitude: Attitude: Eastcoast attitude very straight forward blunt always in a rush. She hate for her time to wasted 			and she will accept nothing but professionalism . shes new to the area but not to medical marijuana, once she finds a 			dispensary she will be a loyal customer</li>
+		<li>Attitude: Attitude: East-coast attitude very straight forward blunt always in a rush. She hate for her time to wasted 			and she will accept nothing but professionalism . shes new to the area but not to medical marijuana, once she finds a 			dispensary she will be a loyal customer</li>
 		<li>Needs/Frustration: Chronic pain being new to the area not knowing where to go</li>
 			<li>Goals:</li>
 		</ol>
@@ -32,11 +32,11 @@
 			<h3>Bill Stoner</h3>
 		<ol>
 			<li>Age: 50</li>
-			<li>Profession: Retired Marine Veterian</li>
-			<li>Technology: Motorla Flip Phone, has Acer ADS560 computer</li>
-			<li>Attitude: Now retired, Bill has an outstanding attitude towards life, loves to fish and camp. Once a month he gets 				toghether with other retired members of the armed forces at Cabezon Wounded Warrior Haven for some real life war 				games. As Bill puts it: "You can take Bill out of the marines, but you will never take the marine out of Bill"</li>
+			<li>Profession: Retired Marine Veteran</li>
+			<li>Technology: Motorola Flip Phone, has Acer ADS560 computer</li>
+			<li>Attitude: Now retired, Bill has an outstanding attitude towards life, loves to fish and camp. Once a month he gets 				together with other retired members of the armed forces at Cabezon Wounded Warrior Haven for some real life war 				games. As Bill puts it: "You can take Bill out of the marines, but you will never take the marine out of Bill"</li>
 			<li>Needs/Frustration:  Like a large percentage of military veterans, Bill suffers from PTSD and other injuries, too many to list.  Bill is a member of the local VFW and even though this has been the primary way he interacts with friends, he is sick and tired of the non-stop beer drinking. Bill wishes there was a better way to stay in touch with friends that served with him.</li>
-			<li>Goals: Bill has heard from several friends how medical cannabis has improved their life by decreasing to a minimum all the effects of ptsd with no side effects. Bill wants to become a patient of the NMMCP and wants a step by step guide on how to acomplish such goal</li>
+			<li>Goals: Bill has heard from several friends how medical cannabis has improved their life by decreasing to a minimum all the effects of ptsd with no side effects. Bill wants to become a patient of the NMMCP and wants a step by step guide on how to accomplish such goal</li>
 		</ol>
 		<h3>MaryJane Verde</h3>
 		<ol>
