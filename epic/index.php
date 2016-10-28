15daad8703fa9809c54a193989bc6d382b665acf
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-28 09:29:17 -0600

    use case

--- epic/index.php
+++ epic/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="">
 
 	<head>
 		<meta charset="UTF-8">
@@ -10,7 +10,7 @@
 
 		<h2>Executive Summary</h2>
 
-		<p>The Cannaduces application will act as a way to connect patients with local dispensaries and offer communinity support. Cannaduceus is focused on Albuquerque's growing population of medical cannabis patients with particular focus on U.S. Veterians and their growing need for medical cannabis. Our target market focuses are 0) U.S Veterians in need of medical cannabis. We want to bring all of the resoruces togehter in an effort to make them more easily accessible. 1) We will be a directory source for people who are not native New Mexicans or patients new to the program. 2) Lastly we want to support and grow a more communal environment between dispensaries and patients while also promoting positive awareness around the benefits of medical cannabis. Our overall goal for Cannaduces is connection and accessibility. We want to put medical cannabis at your finger tips making it easy to find via GPS and helping users connect to the local community of patients around them and all resources specific to thier needs.
+		<p>The Cannaduces application will be act as a medium to connect patients to local dispensaries and communinity support. Cannaduces is based on Albuquerque's large population of U.S. Veterians and its growing market for medical cannabis. Our main target market focuses are 0) U.S Veterians in need of medical cannabis. We want to bring all of the resoruces togehter in an effort to make them more accessible. 1) We also desire to be a directory source for people who are not native New Mexicans'. 2) Lastly we want to support a more communal environment between dispensaries while promoting positive awareness around the needs and benefits of medical cannabis. Our overall goal for Cannaduces is connection and accessibility. We want to put medical cannabis at your finger tips making it easy to find via GPS and helping user connect to the community of patients around them and all resources specific to thier needs.
 		</p>
 
 		<h2>System Goals</h2>
@@ -43,21 +43,24 @@
 			<li>Age: 27 </li>
 			<li>Profession: Kindergarten Teacher</li>
 			<li>Technology: iphone 6s user, LTE, PC user</li>
-			<li>Attitude: Mild mannered teacher with a husband and three year old daughter.  She feels like her life just started and she's already staring death in the face.  Even in this she perseveers and tries to make the best of every day for her daughter.  She receives chemo therapy once a week and it really takes a toil on her.  She uses cannabis for relief and to help her nasuea and lack of appetite.  It also helps her sleep and have more energy to take care of her daughter.</li>
-			<li>Needs/Frustration: Mary Jane needs to be able to use her smart phone to locate the nearest dispensary. Because she doesn't want to travel all over town searching when she is in pain.  She wants to find one close to her house and another close to work as her main ones.  She is hoping to use medical cannabus to help with the symptoms chemo therapy gives her and also increase her appetite so she is able to keep her strength and keep up with her active 3 year old.</li>
-			<li>Goals:Mary Jane needs to be able to use her smart phone to locate the nearest dispensary. Because she doesn't want to travel all over town searching when she is in pain.  She wants to find one close to her house and another close to work as her main ones.  She is hoping to use medical cannabus to help with the symptoms chemo therapy gives her and also increase her appetite so she is able to keep her strength and keep up with her active 3 year old. </li>
+			<li>Attitude: Mild mannered teacher with a husband and three year old daughter.  She feels like her life just started and she's already staring death in the face.  Even in this she perseveres and tries to make the best of every day for her daughter.  She receives chemo therapy once a week and it really takes a toil on her.  She uses cannabis for relief and to help her nausea and lack of appetite.  It also helps her sleep and have more energy to take care of her daughter.</li>
+			<li>Needs/Frustration: Mary Jane needs to be able to use her smart phone to locate the nearest dispensary. Because she doesn't want to travel all over town searching when she is in pain.  She wants to find one close to her house and another close to work as her main ones.  She is hoping to use medical cannabis to help with the symptoms chemo therapy gives her and also increase her appetite so she is able to keep her strength and keep up with her active 3 year old.</li>
+			<li>Goals:Mary Jane needs to be able to use her smart phone to locate the nearest dispensary. Because she doesn't want to travel all over town searching when she is in pain.  She wants to find one close to her house and another close to work as her main ones.  She is hoping to use medical cannabis to help with the symptoms chemo therapy gives her and also increase her appetite so she is able to keep her strength and keep up with her active 3 year old. </li>
 		</ol>
 
 		<h1>Use Case</h1>
-		<li>MaryJane Verde is in the battle of her life. She has very specific needs. Because she has cancer she will more than likely be a mobile user.</li>
+		<p>MaryJane Verde is in the battle of her life. She has very specific needs. Because she has cancer she will more than likely be a mobile user.</p>
 		<ol>
 			<li> MaryJane is new to the idea of medical cannabis, when she spoke with her doctor about alternative options and introduced to the idea of medical cannabis she immediately looked for an application that would provide information on local dispensaries</li>
-			<li> When she searches through the app strore on her iphone she is hoping to find an app that will help her find local dispensaries </li>
+			<li> When she searches through the app store on her iphone she is hoping to find an app that will help her find local dispensaries </li>
 			<li> The Cannaduces app shows and has very good reviews so she downloads the app</li>
-			<li> Once she download she is promted to sign-up as a user or use app as a guest; she choose to use app as a guest so she can fully check the app out</li>
+			<li> Once she download she is promoted to sign-up as a user or use app as a guest; she choose to use app as a guest so she can fully check the app out</li>
 			<li> MaryJane sees that the application has a GPS feature that will give her the location of all the dispensaries within 5, 10, 20 mile radius of her current location</li>
 			<li> Mary Jane looks up all of the dispensaries in her neighborhood and the reviews from other medical cannabis users</li>
-			<li></li>
+			<li> MaryJane finds the app very easy to use and intutive</li>
+			<li> MaryJane later sees the Cannaduces app has several other features she finds useful</li>
+			<li> She browses through the information section and learns information about different strains of medical cannabis</li>
+			<li> She is concerned how she wil consume medical cannabis without having to answer many questions from an inquisitive 3 year old; she finds out she can get all the benefits of the medical cannabis in the form of edibles and even concentrate oils</li>
 		</ol>
 
 	</body>
