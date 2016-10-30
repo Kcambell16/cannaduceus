20df91facbaad1395105b10e33d69f73142fc338
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-30 12:16:38 -0600

    betty baker use case

--- epic/index.php
+++ epic/index.php
@@ -10,7 +10,7 @@
 
 		<h2>Executive Summary</h2>
 
-		<p>The Cannaduces application will be act as a medium to connect patients to local dispensaries and community support. Cannaduces is based on Albuquerque's large population of U.S. Veterans and its growing market for medical cannabis. Our main target market focuses are 0) U.S Veterans in need of medical cannabis. We want to bring all of the resoruces together in an effort to make them more accessible. 1) We also desire to be a directory source for people who are not native New Mexicans'. 2) Lastly we want to support a more communal environment between dispensaries while promoting positive awareness around the needs and benefits of medical cannabis. Our overall goal for Cannaduces is connection and accessibility. We want to put medical cannabis at your finger tips making it easy to find via GPS and helping user connect to the community of patients around them and all resources specific to thier needs.
+		<p>The Cannaduceus application will be act as a medium to connect patients to local dispensaries and community support. Cannaduces is based on Albuquerque's large population of U.S. Veterans and its growing market for medical cannabis. Our main target market focuses are 0) U.S Veterans in need of medical cannabis. We want to bring all of the resoruces together in an effort to make them more accessible. 1) We also desire to be a directory source for people who are not native New Mexicans'. 2) Lastly we want to support a more communal environment between dispensaries while promoting positive awareness around the needs and benefits of medical cannabis. Our overall goal for Cannaduces is connection and accessibility. We want to put medical cannabis at your finger tips making it easy to find via GPS and helping user connect to the community of patients around them and all resources specific to their needs.
 		</p>
 
 		<h2>System Goals</h2>
@@ -53,12 +53,12 @@
 		<ol>
 			<li> MaryJane is new to the idea of medical cannabis, when she spoke with her doctor about alternative options and introduced to the idea of medical cannabis she immediately looked for the mobile website that would provide information on local dispensaries</li>
 			<li> When she searches through browser on her iphone she is hoping to find a mobile friendly website that will help her find local dispensaries </li>
-			<li> The Cannaduces app shows and has very good reviews she finds the Cannaduces app</li>
+			<li> The Cannaduceus app shows and has very good reviews she finds the Cannaduceus app</li>
 			<li> While viewing the mobile website she is promoted to sign-up as a user or use app as a guest; she choose to use app as a guest so she can fully check the app out</li>
 			<li> MaryJane sees that the application has a GPS feature that will give her the location of all the dispensaries within 5, 10, 20 mile radius of her current location</li>
 			<li> Mary Jane looks up all of the dispensaries in her neighborhood and the reviews from other medical cannabis users</li>
 			<li> MaryJane finds the app very easy to use and intutive</li>
-			<li> MaryJane later sees the Cannaduces app has several other features she finds useful</li>
+			<li> MaryJane later sees the Cannaduceus app has several other features she finds useful</li>
 			<li> She browses through the information section and learns information about different strains of medical cannabis</li>
 			<li> She is concerned how she wil consume medical cannabis without having to answer many questions from an inquisitive 3 year old; she finds out she can get all the benefits of the medical cannabis in the form of edibles and even concentrate oils</li>
 			<li> MaryJane has decided to sign up for the application because she sees there are rewards points that can be earned when signing up for app and she knows while she is in chemo therapy she wil be a loyal user.</li>
@@ -66,17 +66,29 @@
 		<p> Bill Stoner is not very tech savvy and his tolerance for frustration is very low, so any technology he uses needs to be very user friendly and simple to navigate.</p>
 		<ol>
 			<li> Bill Stoner has a very basic computer and he is new to using electronics in general, but he has learned to access the internet and and is curious what is available for local medical cannibis users. </li>
-			<li> Bill Stoner has gone to google and searched for websites on medical cannabis in his area Albuquerque, NM and he discovers Cannadueces.</li>
-			<li> Bill navigates to the website and istantly feel relief becuase usally when he is on a website he become very frustrated because he can never figure out where to go.</li>
+			<li> Bill Stoner has gone to google and searched for websites on medical cannabis in his area Albuquerque, NM and he discovers Cannaduceus.</li>
+			<li> Bill navigates to the website and instantly feel relief because usually when he is on a website he become very frustrated because he can never figure out where to go.</li>
 			<li> Bill can quickly see all the areas on the screen he can click and navigate to.</li>
-			<li> Bill has a local dispensary but he is curious about the other locations so he clicks on the section that helps him locate dispensaries in his area. To his suprise he was instanstly prommpted to enter his information</li>
+			<li> Bill has a local dispensary but he is curious about the other locations so he clicks on the section that helps him locate dispensaries in his area. To his surprise he was instantly prompted to enter his information</li>
 			<li> after bill slowly pecked his information into the browser he noticed the enter highlighted enter once all information was complete. He clicks enter and another page pops up with his local dispensaries within a 20 mile radius.</li>
 			<li> At this point Bill is pretty proud of himself because he was finally starting to get a hang of this computer stuff.</li>
 			<li> Bill begins to navigate other tabs he found the most interest in the community tab. This is where he was able to find out all the things going on in the medical cannabis community and he even found other ways to connect to other vets like him. This made him very happy he had been looking for ways to find/ connect with other Vets.</li>
 			<li> Bill noticed there was an community even happening that same afternoon he was looking for something to do to keep in active.</li>
-			<li> Bill even took the time to review his local dispensarie which made him very happy. He loves the workers and htey know him very well so he jumped at the opporutnity to rave about some of his favorite people.</li>
+			<li> Bill even took the time to review his local dispensaries which made him very happy. He loves the workers and htey know him very well so he jumped at the opportunity to rave about some of his favorite people.</li>
 			<li> Bill took a piece of paper out and wrote down the locations and he was even able to get driving directions directly from google to the location of the event.</li>
 			<li> After this experience Bill doesn't feel so out of the loop.</li>
+		</ol>
+		<p> Betty Baker is a woman who is constantly on the move and she knows what she wants and usally how to get it. But Betty is new to Albuquerque, NM, and though she loves it here the stress from all the changes shes has made in the past 3 months has lead to her fibromyalgia acting up and her being in constant pain. She has been prescribed medical cannabis when she was being treated in New York and was lucky enough get her prescription and card switched to New Mexico.</p>
+		<ol>
+			<li>Betty is sitting in on a company meeting, when her fibromyalgia begins to act up. After the meeting she decides that shes tired of feeling so much pain she, pulls out her iphone7s and unlocks and googles medical cannabis. She is prompted to visit a website Cannaduceus</li>
+			<li> What Betty finds is very mobile friendly.</li>
+			<li> Betty choose to view Cannaduceus because the website have many great reviews</li>
+			<li> Betty immediately uses the GPS feature on the website and see there are several dispensaries here her work and one dispensary within her new neighborhood.</li>
+			<li> Betty looks through the local dispensaries and chooses the one that is closest to her at the moment with the best customer reviews</li>
+			<li> Betty is very big on customer experience and she is in pain so wants to make sure the dispensary she visit will provide her with the best experience</li>
+			<li> Betty skims through the website and find the perfect dispensary at at central location between home and work and also has high marks</li>
+			<li> Betty also find interest in the info tab, she was able to find information on local legislative and laws so she woud be able to compare to the state she came from.</li>
+
 		</ol>
 		`
 
