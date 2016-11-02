69aaec848e1003b687689218087fc024f394058a
Nathan Sanchez <nsanchez121@cnm.edu>
2016-11-02 15:34:35 -0600

    w.i.p

--- epic/conceptualmodel.php
+++ epic/conceptualmodel.php
@@ -5,12 +5,19 @@
 		<title>Conceptual Model</title>
 	</head>
 	<body>
+
 		<header>
+
 			<h1>Conceptual Model</h1>
+
 		</header>
+
 		<main>
+
 			<h2>Attributes and Entities</h2>
+
 			<h4>User</h4>
+
 			<ul>
 				<li>email</li>
 				<li>id</li>
@@ -18,14 +25,34 @@
 				<li>userHash</li>
 				<li>userSalt</li>
 			</ul>
+
 			<h2>Dispensary</h2>
+
 			<ul>
 				<li>Id</li>
 				<li>name</li>
 				<li>address</li>
 				<li>e-mail</li>
 				<li>phone #</li>
+				<li>url</li>
 			</ul>
+
+			<h2>dispensaryReview(weak)</h2>
+
+			<ul>
+				<li>dispensaryReviewId</li>
+				<li>reviewTxt</li>
+				<li>dateTime</li>
+			</ul>
+
+			<h2>dispensaryLeafRating</h2>
+
+			<ul>
+				<li>dispenaryLeafratingId</li>
+				<li>dispesaryId</li>
+				<li>userId</li>
+			</ul>
+
 			<h2>Strain</h2>
 			<ul>
 				<li>email</li>
@@ -33,7 +60,7 @@
 				<li>name</li>
 			</ul>
 
-
+			<h2>strain</h2>
 
 
 
