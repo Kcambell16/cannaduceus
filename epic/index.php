f6049e643d9579447f3eb24e062efba11ad2f371
James Montoya <jamesmontoyaarts@gmail.com>
2016-11-02 15:27:00 -0600

    added white space and user stories section

--- epic/index.php
+++ epic/index.php
@@ -18,40 +18,67 @@
 		<p> By using Cannaduceus users will have access to all information regarding medical cannabis at their fingertip. They will be able to gps locate all dispensaries in state, as well as find a wealth of information on the site/ app. Users will be able to review different dispensaries sharing their experience. Users will also have access to local medical cannabis events. There will be a section that keeps patients up to date with all local laws and legislature pertaining to medical cannabis. And lastly for the users over all experience there will be a section that will highlight different products avail such as edible and concentrated oils; as well as
 		</p>
 
-		<h1>Persona</h1>
+		<h1>Personas</h1>
+
 		<h3>Betty Baker</h3>
+
 		<ol>
-		<li>Age: 47</li>
-		<li>Profession: Lawyer</li>
-		<li>Technology: iphone 7s plus, 2012 macbook pro, ipod</li>
-		<li>Attitude: Attitude: East-coast attitude very straight forward blunt always in a rush. She hate for her time to wasted 			and she will accept nothing but professionalism . shes new to the area but not to medical marijuana, once she finds a 			dispensary she will be a loyal customer</li>
-		<li>Needs/Frustration: Chronic pain being new to the area not knowing where to go</li>
+
+			<li>Age: 47</li>
+
+			<li>Profession: Lawyer</li>
+
+			<li>Technology: iphone 7s plus, 2012 macbook pro, ipod</li>
+
+			<li>Attitude: Attitude: East-coast attitude very straight forward blunt always in a rush. She hate for her time to wasted 			and she will accept nothing but professionalism . shes new to the area but not to medical marijuana, once she finds a 			dispensary she will be a loyal customer</li>
+
+			<li>Needs/Frustration: Chronic pain being new to the area not knowing where to go</li>
+
 			<li>Goals: Betty Baker is looking for a app to show her where to go. To help her on her journy to get a medical card in New Mexico, and be able to see the dispensaries near her and what they have.</li>
 		</ol>
 
 			<h3>Bill Stoner</h3>
-		<ol>
-			<li>Age: 50</li>
-			<li>Profession: Retired Marine Veteran</li>
-			<li>Technology: Motorola Flip Phone, has Acer ADS560 computer</li>
-			<li>Attitude: Now retired, Bill has an outstanding attitude towards life, loves to fish and camp. Once a month he gets 				together with other retired members of the armed forces at Cabezon Wounded Warrior Haven for some real life war 				games. As Bill puts it: "You can take Bill out of the marines, but you will never take the marine out of Bill"</li>
-			<li>Needs/Frustration:  Like a large percentage of military veterans, Bill suffers from PTSD and other injuries, too many to list.  Bill is a member of the local VFW and even though this has been the primary way he interacts with friends, he is sick and tired of the non-stop beer drinking. Bill wishes there was a better way to stay in touch with friends that served with him.</li>
-			<li>Goals: Bill has heard from several friends how medical cannabis has improved their life by decreasing to a minimum all the effects of ptsd with no side effects. Bill wants to become a patient of the NMMCP and wants a step by step guide on how to accomplish such goal</li>
-		</ol>
-		<h3>MaryJane Verde</h3>
-		<ol>
-			<li>Age: 27 </li>
-			<li>Profession: Kindergarten Teacher</li>
-			<li>Technology: iphone 6s user, LTE, PC user</li>
-			<li>Attitude: Mild mannered teacher with a husband and three year old daughter.  She feels like her life just started and she's already staring death in the face.  Even in this she perseveres and tries to make the best of every day for her daughter.  She receives chemo therapy once a week and it really takes a toil on her.  She uses cannabis for relief and to help her nausea and lack of appetite.  It also helps her sleep and have more energy to take care of her daughter.</li>
-			<li>Needs/Frustration: Mary Jane needs to be able to use her smart phone to locate the nearest dispensary. Because she doesn't want to travel all over town searching when she is in pain.  She wants to find one close to her house and another close to work as her main ones.  She is hoping to use medical cannabis to help with the symptoms chemo therapy gives her and also increase her appetite so she is able to keep her strength and keep up with her active 3 year old.</li>
-			<li>Goals:Mary Jane needs to be able to use her smart phone to locate the nearest dispensary. Because she doesn't want to travel all over town searching when she is in pain.  She wants to find one close to her house and another close to work as her main ones.  She is hoping to use medical cannabis to help with the symptoms chemo therapy gives her and also increase her appetite so she is able to keep her strength and keep up with her active 3 year old. </li>
-		</ol>
-		<!--User story/ Use Case-->
 
-		<h3> User Story</h3>
+		<ol>
+
+			<li>Age: 50</li>
+
+			<li>Profession: Retired Marine Veteran</li>
+
+			<li>Technology: Motorola Flip Phone, has Acer ADS560 computer</li>
+
+			<li>Attitude: Now retired, Bill has an outstanding attitude towards life, loves to fish and camp. Once a month he gets 				together with other retired members of the armed forces at Cabezon Wounded Warrior Haven for some real life war 				games. As Bill puts it: "You can take Bill out of the marines, but you will never take the marine out of Bill"</li>
+
+			<li>Needs/Frustration:  Like a large percentage of military veterans, Bill suffers from PTSD and other injuries, too many to list.  Bill is a member of the local VFW and even though this has been the primary way he interacts with friends, he is sick and tired of the non-stop beer drinking. Bill wishes there was a better way to stay in touch with friends that served with him.</li>
+
+			<li>Goals: Bill has heard from several friends how medical cannabis has improved their life by decreasing to a minimum all the effects of ptsd with no side effects. Bill wants to become a patient of the NMMCP and wants a step by step guide on how to accomplish such goal</li>
+
+		</ol>
+
+		<h3>MaryJane Verde</h3>
+
+		<ol>
+
+			<li>Age: 27 </li>
+
+			<li>Profession: Kindergarten Teacher</li>
+
+			<li>Technology: iphone 6s user, LTE, PC user</li>
+
+			<li>Attitude: Mild mannered teacher with a husband and three year old daughter.  She feels like her life just started and she's already staring death in the face.  Even in this she perseveres and tries to make the best of every day for her daughter.  She receives chemo therapy once a week and it really takes a toil on her.  She uses cannabis for relief and to help her nausea and lack of appetite.  It also helps her sleep and have more energy to take care of her daughter.</li>
+
+			<li>Needs/Frustration: Mary Jane needs to be able to use her smart phone to locate the nearest dispensary. Because she doesn't want to travel all over town searching when she is in pain.  She wants to find one close to her house and another close to work as her main ones.  She is hoping to use medical cannabis to help with the symptoms chemo therapy gives her and also increase her appetite so she is able to keep her strength and keep up with her active 3 year old.</li>
+
+			<li>Goals:Mary Jane needs to be able to use her smart phone to locate the nearest dispensary. Because she doesn't want to travel all over town searching when she is in pain.  She wants to find one close to her house and another close to work as her main ones.  She is hoping to use medical cannabis to help with the symptoms chemo therapy gives her and also increase her appetite so she is able to keep her strength and keep up with her active 3 year old. </li>
+
+		</ol>
+
+		<!--Mary Jane User story-->
+
+		<h3> Use Case</h3>
 
 		<ol> As a User MaryJane wants to find the closest dispensary to her home</ol>
+
 		<ol>
 			<li> MaryJane is new to the idea of medical cannabis, when she spoke with her doctor about alternative options and introduced to the idea of medical cannabis she immediately looked for the mobile website that would provide information on local dispensaries</li>
 			<li> When she searches through browser on her iphone she is hoping to find a mobile friendly website that will help her find local dispensaries </li>
@@ -65,53 +92,100 @@
 			<li> She is concerned how she wil consume medical cannabis without having to answer many questions from an inquisitive 3 year old; she finds out she can get all the benefits of the medical cannabis in the form of edibles and even concentrate oils</li>
 			<li> MaryJane has decided to sign up for the application because she sees there are rewards points that can be earned when signing up for app and she knows while she is in chemo therapy she wil be a loyal user.</li>
 		</ol>
-		<!-- MaryJane use case-->
+		<!-- MaryJane user story-->
+		<h4> User Story</h4>
 
-		<h4>Use Case</h4>
 		<p>MaryJane Verde is new to the medical cannabis community and wants to learn more about medical cannabis</p>
 
 		<!-- Bill Stoner-->
 
+		<h4> Use Case</h4>
+
 		<p> Bill Stoner is not very tech savvy and his tolerance for frustration is very low, so any technology he uses needs to be very user friendly and simple to navigate.</p>
 
 		<ol>
+
 			<li> Bill Stoner has a very basic computer and he is new to using electronics in general, but he has learned to access the internet and and is curious what is available for local medical cannibis users. </li>
+
 			<li> Bill Stoner has gone to google and searched for websites on medical cannabis in his area Albuquerque, NM and he discovers Cannaduceus.</li>
+
 			<li> Bill navigates to the website and instantly feel relief because usually when he is on a website he become very frustrated because he can never figure out where to go.</li>
+
 			<li> Bill can quickly see all the areas on the screen he can click and navigate to.</li>
+
 			<li> Bill has a local dispensary but he is curious about the other locations so he clicks on the section that helps him locate dispensaries in his area. To his surprise he was instantly prompted to enter his information</li>
+
 			<li> after bill slowly pecked his information into the browser he noticed the enter highlighted enter once all information was complete. He clicks enter and another page pops up with his local dispensaries within a 20 mile radius.</li>
+
 			<li> At this point Bill is pretty proud of himself because he was finally starting to get a hang of this computer stuff.</li>
+
 			<li> Bill begins to navigate other tabs he found the most interest in the community tab. This is where he was able to find out all the things going on in the medical cannabis community and he even found other ways to connect to other vets like him. This made him very happy he had been looking for ways to find/ connect with other Vets.</li>
+
 			<li> Bill noticed there was an community even happening that same afternoon he was looking for something to do to keep in active.</li>
+
 			<li> Bill even took the time to review his local dispensaries which made him very happy. He loves the workers and htey know him very well so he jumped at the opportunity to rave about some of his favorite people.</li>
+
 			<li> Bill took a piece of paper out and wrote down the locations and he was even able to get driving directions directly from google to the location of the event.</li>
+
 			<li> After this experience Bill doesn't feel so out of the loop.</li>
+
 		</ol>
+
 		<!-- Bill Stoner use casw-->
 
-		<h4> Use Case</h4>
 		<ol> As a user Bill Stoner wants to find information on local events that are happening in the community</ol>
 
 		<!--Betty Baker -->
 
+		<h4> Use Case</h4>
+
 		<p> Betty Baker is a woman who is constantly on the move and she knows what she wants and usally how to get it. But Betty is new to Albuquerque, NM, and though she loves it here the stress from all the changes shes has made in the past 3 months has lead to her fibromyalgia acting up and her being in constant pain. She has been prescribed medical cannabis when she was being treated in New York and was lucky enough get her prescription and card switched to New Mexico.</p>
 
 		<ol>
+
 			<li>Betty is sitting in on a company meeting, when her fibromyalgia begins to act up. After the meeting she decides that shes tired of feeling so much pain she, pulls out her iphone7s and unlocks and googles medical cannabis. She is prompted to visit a website Cannaduceus</li>
+
 			<li> What Betty finds is very mobile friendly.</li>
+
 			<li> Betty choose to view Cannaduceus because the website have many great reviews</li>
+
 			<li> Betty immediately uses the GPS feature on the website and see there are several dispensaries here her work and one dispensary within her new neighborhood.</li>
+
 			<li> Betty looks through the local dispensaries and chooses the one that is closest to her at the moment with the best customer reviews</li>
+
 			<li> Betty is very big on customer experience and she is in pain so wants to make sure the dispensary she visit will provide her with the best experience</li>
+
 			<li> Betty skims through the website and find the perfect dispensary at at central location between home and work and also has high marks</li>
+
 			<li> Betty also find interest in the info tab, she was able to find information on local legislative and laws so she woud be able to compare to the state she came from.</li>
 		</ol>
 		<!-- Betty Baker use case-->
 
-		<h4> Use Case</h4>
-		<ol> Betty uses the site to read reviews of local dispensaries to ensure the best experience when visiting.</ol>
-		`
+		<h4> User Story</h4>
 
+		<ol> Betty uses the site to read reviews of local dispensaries to ensure the best experience when visiting.</ol>
+
+		<h4>User Stories</h4>
+
+		<!--Users May/Will Have More Than One Goal-->
+
+		<ul>
+
+			<li>User: Betty Baker - Goal:</li>
+
+			<li>User: Bill Stoner - Goal</li>
+
+			<li>User: Mary Jane - Goal</li>
+
+			<li>User:</li>
+
+			<li>User:</li>
+
+			<li>User:</li>
+
+			<li>User:</li>
+
+		</ul>
+		`
 	</body>
 </html>
