9b71211a8c199a065ba7c9e6b334cd6e27117041
Kelly Campbell <Kcampbell16@cnm.edu>
2016-11-02 15:45:53 -0600

    user goal contin...

--- epic/index.php
+++ epic/index.php
@@ -171,19 +171,23 @@
 
 		<ul>
 
-			<li>User: Betty Baker - Goal: Betty to locate the closest dispensary</li>
+			<li>User: Betty Baker - Goal: To locate the closest dispensary</li>
 
-			<li>User: Bill Stoner - Goal: Bill want to connect with other medical Cannabis users</li>
+			<li>User: Bill Stoner - Goal: Wants to connect with other medical Cannabis users</li>
 
-			<li>User: MaryJane Verde- Goal: MaryJane wants to review strain information. </li>
+			<li>User: MaryJane Verde- Goal: Wants to review strain information. </li>
 
-			<li>User: Betty Baker - Goal: Betty want to read customer review on dispensary </li>
+			<li>User: Betty Baker - Goal: Wants to read customer review on dispensary </li>
 
-			<li>User: Bill Stoner - Goal: Bill wants access to laws and legislative</li>
+			<li>User: Bill Stoner - Goal: Wants access to laws and legislative</li>
 
-			<li>User: MaryJane Verde- Goal: </li>
+			<li>User: MaryJane Verde- Goal: To choose Favorite dispensary</li>
 
-			<li>User:</li>
+			<li>User: Betty Baker - Goal: To write a review different stains used</li>
+
+			<li>User: Bill Stoner -  Goal: To find Community events</li>
+
+			<li>User: MaryJane Verde - Goal: To rate dispensary</li>
 
 		</ul>
 		`
