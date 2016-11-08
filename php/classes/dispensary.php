0776b885cfb8109bf3f00e4233a96e64e7ccc3a9
Kelly Campbell <Kcampbell16@cnm.edu>
2016-11-08 13:38:18 -0700

    correct duplicates

---
+++ php/classes/dispensary.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kcampbell
+ * Date: 11/8/16
+ * Time: 1:15 PM
+ */
+
+namespace Edu\Cnm\kcampbell16\cannaduceus;
+/**
+ * Cross section of cannaduceus dispensary class
+ *
+ * This is just one dispensary  out of many that will be reviewed and catagorized
+ *
+ * @author Kelly Campbell <kcampbell16@cnm.edu>
+ *
+ **/
+
+
+class Dispensary {
+
+	/**
+	 * id for the strain; this is the primary key
+	 * @var int $dispensaryId
+	 **/
+	private $dispensaryId;
+
+	/**
+	 * attention to specific dispensary
+	 * @var string $dispensaryAttention
+	 */
+	private $dispensaryAttention;
+
+	/**
+	 * actual city of dispensary
+	 * @var string $dispensaryCity
+	 **/
+	private $dispensaryCity;
+
+	/**
+	 * actual dispensary email address
+	 * @var string $dispensaryEmail
+	 **/
+	private $dispensaryEmail;
+
+	/**
+	 * favorites for dispensary give by users
+	 * @var int $dispensaryFavorite
+	 **/
+	private $dispensaryFavorite
+
+	/**
+	 * actual name of dispensary
+	 * @var string $dispensaryName
+	 **/
+	private $dispensaryName
+
+		/**
+		 * actual name of dispensary
+		 * @var int $dispensaryName
+		 **/
+
+}
