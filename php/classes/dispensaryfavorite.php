7c075c9a55f0322a91af1200f45618e542452089
Nathan Sanchez <nsanchez121@cnm.edu>
2016-11-09 11:48:25 -0700

    https://app.asana.com/0/201605659938138/206915832495081 public function __construct

--- php/classes/dispensaryfavorite.php
+++ php/classes/dispensaryfavorite.php
@@ -36,6 +36,22 @@ class DispensaryFavorite {
 	 */
 
 	public function __construct(int $newDispensaryFavoriteId = null, int $newDispensaryFavoriteDispensaryId) {
+		try {
+			$this->dispensaryFavoriteProfileId($newDispensaryFavoriteId);
+			$this->dispensaryFavoritedispensaryId($newDispensaryFavoriteDispensaryId);
+		}Catch(\InvalidArgumentException $invalidArgumentException) {
+			// rethrow the exception to the calller
+			throw(new \InvalidArgumentException($invalidArgumentException->getMessage(), 0, $invalidArgumentException));
+		}Catch(\RangeException $range) {
+			// rethrow the exception to the caller
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		}Catch(\TypeError $typeError) {
+			//rethrow the exception to the caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+			} Catch(\Exception $exception) {
+			//rethrow the exception to the caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
 	}
 
 
