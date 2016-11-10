8141f89a3df7af8d0b6203593bd814d715b63d29
James Montoya <jamesmontoyaarts@gmail.com>
2016-11-09 18:40:37 -0700

    created profileUnitTest.php
    https://app.asana.com/0/201605659938138/209626059880087

---
+++ php/test/profileUnitTest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Edu\Cnm\Cannaduceus\Test;
+
+use Edu\Cnm\Cannaduceus\{Profile};
+
+//grabs the project parameters
+require_once ("CannaduceusTest.php");
+
+//grabs the class being tested
+require_once (dirname(__DIR__) . "/public_html/php/classes/autoload.php");
+
+/**
+ * Full PHPUnit test for the Profile class
+ * all MySQL/PDO enabled methods are tested for both invalid and valid inputs in this test
+ * @see Profile
+ * @author James Montoya <jamesmontoyaarts@gmail.com>
+ */
+
+class Profile extends CannaduceusTest{
+	//adding this to test dummy profile
+	protected $profile = null;
+
+	//adding this to test
+	protected
+}
