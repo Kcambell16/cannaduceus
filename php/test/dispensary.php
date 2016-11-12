15b6a827c77a2465e4600c22240e3e88dbca28cf
Kelly Campbell <Kcampbell16@cnm.edu>
2016-11-11 18:47:02 -0700

    starting dispensary unit test

---
+++ php/test/dispensary.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kcampbell
+ * Date: 11/11/16
+ * Time: 6:45 PM
+ */
+
+namespace Edu\Cnm\Cannaduceus\Test;
+
+use Edu\Cnm\Cannaduceus\{Dispensary};
+
+//grabs the project parameters
+require_once ("CannaduceusTest.php");
+
+//grabs the class being tested
+require_once (dirname(__DIR__) . "/public_html/php/classes/autoload.php");
+
+/**
+ * Full PHPUnit test for the Profile class
+ * all MySQL/PDO enabled methods are tested for both invalid and valid inputs in this test
+ * @see Profile
+ * @author Kelly Campbell <kcampbell16@cnm.edu>
+ */
+
+class dispensary {
+
+}
