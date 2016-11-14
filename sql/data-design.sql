0fad3d45a21109444c124066cd99910344acdb97
Nathan Sanchez <nsanchez121@cnm.edu>
2016-11-14 11:38:40 -0700

    fixed some errors in data-design https://app.asana.com/0/201605659938138/209626059880083

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -14,13 +14,11 @@ CREATE TABLE profile
 	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	profileUserName VARCHAR(32) NOT NULL,
 	profileEmail VARCHAR(128) NOT NULL,
-	profileHash VARCHAR(32) NOT NULL,
-	profileSalt VARCHAR(32) NOT NULL,
-	profileActivation VARCHAR(32) NOT NULL,
+	profileHash CHAR(128) NOT NULL,
+	profileSalt CHAR(64) NOT NULL,
+	profileActivation CHAR(32) NOT NULL,
 	UNIQUE (profileUserName),
 	UNIQUE (profileEmail),
-	INDEX (profileUserName),
-	INDEX (profileEmail),
 	PRIMARY KEY (profileId)
 );
 
@@ -29,11 +27,11 @@ CREATE TABLE dispensary
 	dispensaryId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	dispensaryName VARCHAR(32) NOT NULL,
 	dispensaryAttention VARCHAR(32),
-	dispensaryStreet1 VARCHAR(32) NOT NULL,
-	dispensaryStreet2 VARCHAR(32),
-	dispensaryCity VARCHAR(32) NOT NULL,
-	dispensaryState VARCHAR(32) NOT NULL ,
-	dispensaryZipCode VARCHAR(32) NOT NULL,
+	dispensaryStreet1 VARCHAR(48) NOT NULL,
+	dispensaryStreet2 VARCHAR(48),
+	dispensaryCity VARCHAR(48) NOT NULL,
+	dispensaryState CHAR(2) NOT NULL ,
+	dispensaryZipCode VARCHAR(10) NOT NULL,
 	dispensaryEmail VARCHAR(128) NOT NULL,
 	dispensaryPhone VARCHAR(32) NOT NULL,
 	dispensaryUrl VARCHAR(128),
@@ -44,10 +42,10 @@ CREATE TABLE strain
 (
 	strainId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	strainName VARCHAR(32) NOT NULL,
-	strainDescription VARCHAR(128) NOT NULL,
-	strainType VARCHAR(32) NOT NULL,
-	strainThc VARCHAR(32) NOT NULL,
-	strainCbd VARCHAR(32) NOT NULL,
+	strainDescription VARCHAR(255) NOT NULL,
+	strainType VARCHAR(16) NOT NULL,
+	strainThc DECIMAL(8,6) NOT NULL,
+	strainCbd DECIMAL(8,6) NOT NULL,
 	PRIMARY KEY (strainId)
 );
 
@@ -57,7 +55,7 @@ CREATE TABLE dispensaryReview
 	dispensaryReviewProfileId INT UNSIGNED NOT NULL,
 	dispensaryReviewDispensaryId INT UNSIGNED NOT NULL,
 	dispensaryReviewDateTime INT UNSIGNED NOT NULL,
-	dispensaryReviewTxt VARCHAR(256) NOT NULL,
+	dispensaryReviewTxt VARCHAR(255) NOT NULL,
 	INDEX (dispensaryReviewProfileId),
 	INDEX (dispensaryReviewDispensaryId),
 	FOREIGN KEY (dispensaryReviewProfileId) REFERENCES profile (profileId),
@@ -69,7 +67,7 @@ CREATE TABLE dispensaryLeafRating
 (
 	dispensaryLeafRatingDispensaryId INT UNSIGNED NOT NULL,
 	dispensaryLeafRatingProfileId INT UNSIGNED NOT NULL,
-	dispensaryLeafRatingRating INT UNSIGNED NOT NULL,
+	dispensaryLeafRatingRating TINYINT UNSIGNED NOT NULL,
 	INDEX (dispensaryLeafRatingDispensaryId),
 	INDEX (dispensaryLeafRatingProfileId),
 	FOREIGN KEY (dispensaryLeafRatingProfileId) REFERENCES profile (profileId),
@@ -77,7 +75,7 @@ CREATE TABLE dispensaryLeafRating
 	PRIMARY KEY (dispensaryLeafRatingProfileId, dispensaryLeafRatingDispensaryId)
 );
 
-CREATE TABLE dispensaryfavorite
+CREATE TABLE dispensaryFavorite
 (
 	dispensaryFavoriteProfileId INT UNSIGNED NOT NULL,
 	dispensaryFavoriteDispensaryId INT UNSIGNED NOT NULL,
@@ -94,7 +92,7 @@ CREATE TABLE strainReview
 	strainReviewProfileId INT UNSIGNED NOT NULL,
 	strainReviewStrainId INT UNSIGNED NOT NULL,
 	strainReviewDateTime VARCHAR(32),
-	strainReviewTxt VARCHAR (256),
+	strainReviewTxt VARCHAR (255),
 	INDEX (strainReviewProfileId),
 	INDEX (strainReviewStrainId),
 	FOREIGN KEY (strainReviewProfileId) REFERENCES profile (profileId),
@@ -108,15 +106,15 @@ CREATE TABLE strainLeafRating
 (
 	strainLeafRatingStrainId INT UNSIGNED NOT NULL,
 	strainLeafRatingProfileId INT UNSIGNED NOT NULL,
-	strainLeafRatingRating INT UNSIGNED NOT NULL,
+	strainLeafRatingRating TINYINT UNSIGNED NOT NULL,
 	INDEX (strainLeafRatingStrainId),
 	INDEX (strainLeafRatingProfileId),
 	FOREIGN KEY (strainLeafRatingProfileId) REFERENCES profile (profileId),
 	FOREIGN KEY (strainLeafRatingStrainId) REFERENCES  strain (strainId),
-	PRIMARY KEY (strainLeafRatingProfileId, strainLeafRatingRating)
+	PRIMARY KEY (strainLeafRatingProfileId, strainLeafRatingStrainId)
 );
 
-CREATE TABLE strainfavorite
+CREATE TABLE strainFavorite
 (
 	strainFavoriteProfileId INT UNSIGNED NOT NULL,
 	strainFavoriteStrainId INT UNSIGNED NOT NULL,
