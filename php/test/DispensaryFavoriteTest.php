d1034c2673c4c9347d207bcbaf2c815648670886
Nathan Sanchez <nsanchez121@cnm.edu>
2016-11-14 09:06:47 -0700

    https://app.asana.com/0/201605659938138/209699402284638 getting the file for dispensary favorites set up namespacing etc

---
+++ php/test/DispensaryFavoriteTest.php
@@ -0,0 +1,8 @@
+<?php
+namespace Edu\Cnm\nsanchez121\Cannaduceus\Test;
+
+use Edu\Cnm\nsanchez121\Cannaduceus\{Dispensary, review};
+
+// grab the test parameters
+require_once("DispensaryFavoriteTest.php");
+
