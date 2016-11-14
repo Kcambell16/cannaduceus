69d7f2cf17bef48269b2e7c9e974c12aedc208dd
Nathan Sanchez <nsanchez121@cnm.edu>
2016-11-14 10:26:43 -0700

    https://app.asana.com/0/201605659938138/209699402284638 got the class and what not added stay tuned for more updates ( ͡° ͜ʖ ͡°)

--- php/test/DispensaryFavoriteTest.php
+++ php/test/DispensaryFavoriteTest.php
@@ -1,8 +1,26 @@
 <?php
 namespace Edu\Cnm\nsanchez121\Cannaduceus\Test;
 
+use Edu\Cnm\Cannaduceus\Test\CannaduceusTest;
 use Edu\Cnm\nsanchez121\Cannaduceus\{Dispensary, review};
 
 // grab the test parameters
 require_once("DispensaryFavoriteTest.php");
 
+require_once(dirname(__DIR__) . "/classes/DispensaryFavorite.php");
+
+/**
+ * Full PHPUnit test for the dispensary favorite class
+ *
+ * this is a complete PHPUnit test of the dispensary favorite class. It is complete becasue *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and vailid inputs.
+ *
+ * @see Review
+ * @author Nathan Sanchez <nsanchez121@cnm.edu>
+ */
+class DispensaryFavoriteTest extends CannaduceusTest {
+	/**
+	 * dispensary of the favorite
+	 * @var string $VALID_
+	 */
+}
