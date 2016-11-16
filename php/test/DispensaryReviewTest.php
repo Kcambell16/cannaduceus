52dc21ca7a0f53290dd69c25f28e1518e6b85790
Hector Lozano <hlozano2@cnm.edu>
2016-11-16 16:55:43 -0700

    began coding in dispensary review test
    https://app.asana.com/0/201605659938138/209699402284628

--- php/test/DispensaryReviewTest.php
+++ php/test/DispensaryReviewTest.php
@@ -1,7 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hectorlozano
- * Date: 11/16/16
- * Time: 11:46 AM
- */
+namespace Edu\Cnm\hlozano2\DataDesign\Test;
+
+use Edu\Cnm\hlozano2\DataDesign\{Profile, Tweet};
+
+// grab the project test parameters
+require_once("DataDesignTest.php");
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/classes/autoload.php");
