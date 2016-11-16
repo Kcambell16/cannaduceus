dcf55f72d82f226d96e0b5c8e565cade896e4742
Hector Lozano <hlozano2@cnm.edu>
2016-11-16 16:59:47 -0700

    completed coding in dispensary review test up to line 20
    https://app.asana.com/0/201605659938138/209699402284628

--- php/test/DispensaryReviewTest.php
+++ php/test/DispensaryReviewTest.php
@@ -1,10 +1,20 @@
 <?php
 namespace Edu\Cnm\hlozano2\DataDesign\Test;
 
-use Edu\Cnm\hlozano2\DataDesign\{Profile, Tweet};
+use Edu\Cnm\hlozano2\DataDesign\{Profile, DispensayReview};
 
 // grab the project test parameters
 require_once("DataDesignTest.php");
 
 // grab the class under scrutiny
 require_once(dirname(__DIR__) . "/classes/autoload.php");
+
+/**
+ * Full PHPUnit test for the DispensaryReview class
+ *
+ * This is a complete PHPUnit test of the DispensaryReview class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see DispensayReview
+ * @author Hector Lozano <hlozano2@cnm.edu>
+ **/
