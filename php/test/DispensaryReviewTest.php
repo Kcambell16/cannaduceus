827fbbbf973ef8648ba51f11c05d0145f32a7325
Hector Lozano <hlozano2@cnm.edu>
2016-11-16 17:18:56 -0700

    completed coding in dispensary review test up to line 74
    https://app.asana.com/0/201605659938138/209699402284628

--- php/test/DispensaryReviewTest.php
+++ php/test/DispensaryReviewTest.php
@@ -1,7 +1,7 @@
 <?php
 namespace Edu\Cnm\hlozano2\DataDesign\Test;
 
-use Edu\Cnm\hlozano2\DataDesign\{Profile, DispensayReview};
+use Edu\Cnm\hlozano2\DataDesign\{DispensayReviewProfileId, DispensayReview};
 
 // grab the project test parameters
 require_once("DataDesignTest.php");
@@ -36,7 +36,7 @@ class DispensaryReviewTest extends DataDesignTest {
 	protected $VALID_DISPENSARYREVIEWDATE = null;
 	/**
 	 * Profile that created the DispensaryReview; this is for foreign key relations
-	 * @var Profile profile
+	 * @var DispensayReviewProfileId profile
 	 **/
 	protected $profile = null;
 
@@ -53,4 +53,22 @@ class DispensaryReviewTest extends DataDesignTest {
 
 		// calculate the date (just use the time the unit test was setup...)
 		$this->VALID_DISPENSARYREVIEWDATE = new \DateTime();
+	}
+	/**
+	 * test inserting a valid DispensaryReview and verify that the actual mySQL data matches
+	 **/
+	public function testInsertValidDispensaryReview() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("dispensary review");
+
+		// create a new DispensaryReview and insert to into mySQL
+		$dispensaryReview = new DispensayReview(null, $this->profile->dispensaryReviewProfileId(), $this->VALID_DISPENSARYREVIEWCONTENT, $this->VALID_DISPENSARYREVIEWDATE);
+		$dispensaryReview->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoTweet = Tweet::getTweetByTweetId($this->getPDO(), $tweet->getTweetId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
+		$this->assertEquals($pdoTweet->getProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT);
+		$this->assertEquals($pdoTweet->getTweetDate(), $this->VALID_TWEETDATE);
 	}
