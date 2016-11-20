7b95a9ae9c8da85c37ffa9fa6a318a32c0ea4e8f
Nathan Sanchez <nsanchez121@cnm.edu>
2016-11-20 15:45:18 -0700

    getting this stuff started https://app.asana.com/0/201605659938138/209699402284638

--- php/test/DispensaryFavoriteTest.php
+++ php/test/DispensaryFavoriteTest.php
@@ -1,12 +1,13 @@
 <?php
 namespace Edu\Cnm\Cannaduceus\Test;
 
-use Edu\Cnm\Cannaduceus\Test\CannaduceusTest;
-use Edu\Cnm\Cannaduceus\{Dispensary, review};
+use Edu\Cnm\Cannaduceus\Dispensary;
+use Edu\Cnm\Cannaduceus\Profile;
+use Edu\Cnm\Cannaduceus\Test\{DispensaryFavorite};
 
 // grab the test parameters
 require_once("DispensaryFavoriteTest.php");
-
+// grab the class being tested
 require_once(dirname(__DIR__) . "/classes/autoload.php");
 
 /**
@@ -15,20 +16,22 @@ require_once(dirname(__DIR__) . "/classes/autoload.php");
  * this is a complete PHPUnit test of the dispensary favorite class. It is complete becasue *ALL* mySQL/PDO enabled methods
  * are tested for both invalid and vailid inputs.
  *
- * @see Review
- * @author Nathan Sanchez <nsanchez121@cnm.edu>
+ * @see DispensaryFavorite
+ * @author nathan sanchez <nsanchez121@cnm.edu>
  */
 class DispensaryFavoriteTest extends CannaduceusTest {
+	/*--------------------------------Declare Protected State Variables -----------------------*/
+
 	/**
 	 * the favorite dispensary
-	 * @var string $VALID_DISPENSARYFAVORITE
+	 * @var string $VALID_DISPENSARYFAVORITE1
 	 */
-	protected $VALID_FAVORITEDISPENSARY = "PHPUnit test passing";
+	protected $VALID_FAVORITEDISPENSARY1 = "FavoriteDispensary";
 	/**
 	 * content of the updated Favorite
 	 * @var string $VALID_DISPENSARYFAVORITE2
 	 */
-	protected $VAILD_FAVORITEDISPENSARY2 = "PHPUnit test still passing";
+	protected $VAILD_FAVORITEDISPENSARY2 = "FavoriteDispensaryUpdated";
 	/**
 	 * Profile that created the Favorite; this is foreign key relations
 	 * @var
@@ -38,4 +41,61 @@ class DispensaryFavoriteTest extends CannaduceusTest {
 	/**
 	 * create dependent objects before running each test
 	 */
+	public final function setUp() {
+		// run the default abstract setUp() method from parent first
+		parent::setUp();
+
+
+		// create and insert a Profile to own the test DispensaryFavorite
+		$password = "abc123";
+		$salt = bin2hex(random_bytes(32));
+		$hash = hash_pbkdf2("sha512", $password, $salt, 262144, 128);
+		$activation = bin2hex(random_bytes(16));
+		$this->profile = new Profile(null, "profileUserName", "profileEmail", "profileHash", "profileSalt", "profileActivation");
+		$this->profile -> insert($this->getPDO());
+		// create and insert a Dispensary to Favorite the test DispensaryFavorite
+		$this-> dispensary = new Dispensary(null, "dispensaryAttention", "dispensaryCity", "dispensaryEmail", "dispensaryName", "dispensaryPhone", "dispensaryState", "dispensaryStreet1", "dispensaryStreeet2","dispensaryUrl", "dispensaryZipCode");
+		$this-> dispensary->insert($this->getPDO());
+	}
+
+	public function testInsertValidDispensaryFavorite() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("dispensary favorite");
+
+
+		// create a new DispensaryFavorite and insert it in to mySQL
+		$dispensaryFavorite = DispensaryFavorite(null, $this->profile->getProfileId(), $this->VALID_FAVORITEDISPENSARY1, $this->VAILD_FAVORITEDISPENSARY2);
+		$dispensaryFavorite->insert($this->getPDO());
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
