68e167613926c233c3f6ecf83f315304abc24d3b
Kelly Campbell <Kcampbell16@cnm.edu>
2016-11-28 09:14:50 -0700

    adding strain view to angular set up

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,10 +1,12 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
+import {StrainComponent} from "./components/strain-component";
 
-export const allAppComponents = [HomeComponent];
+export const allAppComponents = [HomeComponent, StrainComponent];
 
 export const routes: Routes = [
-	{path: "", component: HomeComponent}
+	{path: "", component: HomeComponent},
+	{path: "strain", component: StrainComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,7 +1,7 @@
 <section class="welcome">
 	<div class="container">
 		<div class="jumbotron text-center">
-			<h1>Welcome to the Angular 2 Demo!</h1>
+			<h1>Welcome to the Home</h1>
 			<p class="lead">This is a bare-bones Angular 2 Front End :D</p>
 		</div>
 	</div>
