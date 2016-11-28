7251fdcb98a16573f8097ce2bae51453a8f70f44
Hector Lozano <hlozano2@cnm.edu>
2016-11-28 09:58:37 -0700

    finished writing strain reviews

--- epic/strainReviews.php
+++ epic/strainReviews.php
@@ -7,10 +7,16 @@
 
 	<body>
 		<h2>Berry Bomb</h2>
-			<p>"If what you are after is to slip into a coma, Berry Bomb is The Bomb!!!  Shortly after your first dose, a tingling sensation takes over your body and it begins to relax, and then your toes begin to curl.  At this point you only have a few minutes before it's lights out. Three hours later I woke up totally rested and full of energy.</p>
+			<p>If what you are after is to slip into a coma, Berry Bomb is The Bomb!!!  Shortly after your first dose, a tingling sensation takes over your body and it begins to relax, and then your toes begin to curl.  At this point you only have a few minutes before it's lights out. Three hours later I woke up totally rested and full of energy.</p>
 
 		<h2>Harlequin</h2>
 			<p>I have mixed reviews about this strain and it's not by the strains fault. As a PTSD patient, I know I should stay away from sativa dominant strains, however, I've had good results with some of the hybrid eatibles. Harlequin helped my pain and anxiety but I did experience a moderate level of paranoia and a hypersensitivity to some sounds.</p>
+
+		<h2>Zak Haze</h2>
+			<p>I use this strain as a preventative mesure for cancer. Zak Haze is high in CBG-A which happens to be an Anti-Proliferative (Inhibits cancer cell growth through apoptosis). Zak also provides me with a happy elevated activity mood which allows me to get the shores done at home</p>
+
+		<h2>Mendocino Purple</h2>
+			<p>Awesome Sleep Aid! Great Stress and Anxiety Inhibitor! I really enjoyed the gradual onset of the medicinal effects of what I now call "My Purple Friend" I took a large bud and put it in a tea ball and made the best sleep tea ever! Try it! You won't regret it.</p>
 
 
 	</body>
