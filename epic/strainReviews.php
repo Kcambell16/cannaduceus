32420a87bf54c791274735495f33eb82d71a0e6f
Hector Lozano <hlozano2@cnm.edu>
2016-11-28 09:13:34 -0700

    beggan writing strain reviews

---
+++ epic/strainReviews.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Strain Reviews</title>
+	</head>
+
+	<body>
+		<h2>Berry Bomb</h2>
+			<p>"If what you are after is to slip into a coma, Berry Bomb is The Bomb!!!  Shortly after your first dose, a tingling sensation takes over your body and it begins to relax, and then your toes begin to curl.  At this point you only have a few minutes before it's lights out. Three hours later I woke up totally rested and full of energy.</p>
+
+		<h2>Harlequin</h2>
+			<p>I have mixed reviews about this strain and it's not by the strains fault. As a PTSD patient, I know I should stay away from sativa dominant strains, however, I've had good results with some of the hybrid eatibles. Harlequin helped my pain and anxiety but I did experience a moderate level of paranoia and a hypersensitivity to some sounds.</p>
+
+
+	</body>
+</html>
