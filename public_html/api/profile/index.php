a3dca5447746405cf06dee7c37b923ce36a94726
Nathan Sanchez <nsanchez121@cnm.edu>
2016-11-29 16:43:38 -0700

    ;)

--- public_html/api/profile/index.php
+++ public_html/api/profile/index.php
@@ -29,54 +29,40 @@ $reply->data = null;
 
 
 try {
-			//grab the mySQL DataBase connection
-			$pdo = connectToEncryptedMySQL("/etc/apache2/cannaduceus/profile.ini");
+	//grab the mySQL DataBase connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/cannaduceus/profile.ini");
 
 
+	//determines which HTTP Method needs to be processed and stores the result in $method.
+	$method = array_key_exists("HTTP_x_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
-			//determines which HTTP Method needs to be processed and stores the result in $method.
-			$method = array_key_exists("HTTP_x_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
-
-			//stores the Primary Key for the GET, DELETE, and PUT methods in $id. This key will come in the URL sent by the front end. If no key is present, $id will remain empty. Note that the input is filtered.
-			$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
-
+	//stores the Primary Key for the GET, DELETE, and PUT methods in $id. This key will come in the URL sent by the front end. If no key is present, $id will remain empty. Note that the input is filtered.
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 
 
-			//Here we check and make sure that we have the Primary Key for the DELETE and PUT requests. If the request is a PUT or DELETE and no key is present in $id, An Exception is thrown.
-			if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
-				throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+	//Here we check and make sure that we have the Primary Key for the DELETE and PUT requests. If the request is a PUT or DELETE and no key is present in $id, An Exception is thrown.
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
 
 // Here, we determine if the reques received is a GET request
-if($method === "GET") {
+	if($method === "GET") {
 		//set XSRF cookie
 		setXsrfCookie("/");
 
 		// Here, we determine if a Key was sent in the URL by checking $id. If so, we pull the requested Profile by Profile ID from the DataBase and store it in $profile.
 		if(empty($profileId) === false) {
-		$profile = Profile::getProfileByProfileId($pdo, $profileId);
-		if($profile !== null) {
-			$reply->data = $profile;
-			// here we store the $profile in the $reply->data state variable
+			$profile = Profile::getProfileByProfileId($pdo, $profileId);
+			if($profile !== null) {
+				$reply->data = $profile;
+				// here we store the $profile in the $reply->data state variable
+			}
+		} else if(empty($profileUsername) === false) {
+			$profile = Profile::getProfilebyProfileUserName($pdo, $profileId);
+			if($profile !== null) {
+				$reply->data = $profile;
+			}
 		}
-	} else if(empty($profileUsername) === false) {
-	$profile = Profile::getProfilebyProfileUserName($pdo, $profileId);
-		if($profile !== null) {
-			$reply->data = $profile;
-		}
-	} else if (empty($profileEmail) === false) {
-		$profile = Profile::getProfileByProfileEmail($pdo, $profileId);
-		if($profile !== null) {
-			$reply->data = $profile;
-		}
-	}
-
-
-
-
-
-
-
 
 
 	}
