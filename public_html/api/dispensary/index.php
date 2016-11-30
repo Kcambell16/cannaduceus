d9186dba1febf6192cbc9ca27859ac1520cc26f4
Kelly Campbell <Kcampbell16@cnm.edu>
2016-11-30 10:11:53 -0700

    completed api for dispensary class GET only needed for class

--- public_html/api/dispensary/index.php
+++ public_html/api/dispensary/index.php
@@ -1,16 +1,16 @@
 <?php
-require_once dirname(__DIR__, 3 ) . "/php/classes/autoloader.php";
-require_once dirname(__DIR__, 3 ) . "/php/lib/xsrf.php";
+
+require_once dirname(__DIR__, 3) . "php/classes/autoloader.php";
+require_once dirname(__DIR__, 3) . "php/lib/xsrf.php";
 require_once "/etc/apache2/cannaduceus/encrypted-config.php";
 
-use Edu\Cnm\Cannaduceus;
+use Edu\Cnm\Cannaduceus\Dispensary;
 
 /**
  * * api for Dispensary class
  *
  * @author Derek Mauldin <derek.e.mauldin@gmai.com>
  **/
-
 
 
 // Check the session status. If it is not active, start the session.
@@ -28,8 +28,7 @@ $reply->data = null;
 
 try {
 	//grab the mySQL DataBase connection
-	$pdo = connectToEncryptedMySQL("/etc/apache2/cannaduceus/dispensary.ini");
-
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cannaduceus.ini");
 
 
 	//determines which HTTP Method needs to be processed and stores the result in $method.
@@ -37,11 +36,65 @@ try {
 
 	//stores the Primary Key for the GET, DELETE, and PUT methods in $id. This key will come in the URL sent by the front end. If no key is present, $id will remain empty. Note that the input is filtered.
 	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
-
+	$name = filter_input(INPUT_GET, "dispensary name", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$all =  filter_input(INPUT_GET, "all dispensaries", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 
 	//Here we check and make sure that we have the Primary Key for the DELETE and PUT requests. If the request is a PUT or DELETE and no key is present in $id, An Exception is thrown.
-	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+	if(($method === "DELETE" || $method === "PUT" || $method === "POST") && (empty($id) === true || $id < 0)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
+
+	// Here, we determine if the request received is a GET request
+	if($method === "GET") {
+		//set XSRF cookie
+		setXsrfCookie("/");
+		// handle GET request - if id is present, that tweet is present, that tweet is returned, otherwise all tweets are returned
+
+
+		// Here, we determine if a Key was sent in the URL by checking $id. If so, we pull the requested Tweet by Tweet ID from the DataBase and store it in $tweet.
+		if(empty($id) === false) {
+			$dispensary = Dispensary::getDispensariesByDispensaryId($pdo, $id);
+			if($dispensary !== null) {
+				$reply->data = $dispensary;
+				// Here, we store the retrieved Dispensary in the $reply->data state variable.
+			} else if(empty($dispensaryId) === false) {
+				$dispensary = Dispensary::getDispensariesByDispensaryName($pdo, $dispensaryId);
+				if($dispensaries !== null) {
+					$reply->data = $dispensaries;
+				}
+			}
+		} else if(empty($all) === false) {
+			$dispensaries = Dispensary::getAllDispensaries($pdo, $all);
+			if($dispensaries !== null) {
+				$reply->data = $dispensaries;
+			}
+		}
+	} else if(empty($name) === false) {
+		$dispensary = Dispensary::getDispensaryByDispensaryName($pdo, $name);
+		if($dispensary !== null) {
+			$reply->data = $dispensary;
+		}
+	}
+	else{
+		throw (new InvalidArgumentException("invalid HTTP method request"));
+	}
+// If there is nothing in $id, and it is a GET request, then we simply return all tweets. We store all the tweets in the $tweets varable, and then store them in the $reply->data state variable.
+
+
+
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
 }
+
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+
+// encode and return reply to front end caller
+echo json_encode($reply);
