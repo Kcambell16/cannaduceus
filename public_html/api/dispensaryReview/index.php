ad9e77f61ff5439434659db71f7741df3ce8c839
Hector Lozano <hlozano2@cnm.edu>
2016-11-30 20:07:03 -0700

    added more stuff to restfulapi for dispensary review
    https://app.asana.com/0/201605659938138/220765076434886

--- public_html/api/dispensaryReview/index.php
+++ public_html/api/dispensaryReview/index.php
@@ -1,9 +1,9 @@
 <?php
-require_once dirname(__DIR__, 3 ) . "/php/classes/autoloader.php";
-require_once dirname(__DIR__, 3 ) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3 ) . "/php/classes/autoload.php";
+//require_once dirname(__DIR__, 3 ) . "/php/lib/xsrf.php";
 require_once "/etc/apache2/capstone-mysql/encrypted-config.php";
 
-use Edu\Cnm\Cannaduceus;
+use Edu\Cnm\Cannaduceus\DispensaryReview;
 
 /**
  * * api for DispensaryReview class
@@ -27,14 +27,15 @@ $reply->data = null;
 
 try {
 	//grab the mySQL DataBase connection
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/dispensaryReview.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cannaduceus.ini");
 
 
 	//determines which HTTP Method was used.
 	$method = array_key_exists("HTTP_x_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	$reply->method = $method;
 
 	//stores the Primary Key for the GET, DELETE, and PUT methods in $id. This key will come in the URL sent by the front end. If no key is present, $id will remain empty. Note that the input is filtered. Sanitize input.
-	$dispensaryReviewid = filter_input(INPUT_GET, "dispensaryReviewId", FILTER_VALIDATE_INT);
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 	$dispensaryReviewProfileId = filter_input(INPUT_GET, "dispensaryReviewProfileId", FILTER_VALIDATE_INT);
 	$dispensaryReviewDispensaryId = filter_input(INPUT_GET, "content", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
@@ -48,33 +49,33 @@ try {
 // Here, we determine if the request received is a GET request
 	if($method === "GET") {
 		//set XSRF cookie
-		setXsrfCookie("/");
+	//	setXsrfCookie("/");
 		// handle GET request - if id is present, that dispensaryReview is present, that dispensaryReview is returned, otherwise all dispensaryReviews are returned
-
+	//$reply->message = 'inside get';
 
 		// Here, we determine if a Key was sent in the URL by checking $id. If so, we pull the requested DispensaryReview by DispensaryReview ID from the DataBase and store it in $dispensaryReview.
 		if(empty($dispensaryReviewId) === false) {
-			$dispensaryReview = DispensaryReview::getDispensaryReviewByDispensaryReviewId($pdo, $id);
-			if($dispensaryReview !== null) {
-				$reply->data = $dispensaryReview;
+				$dispensaryReview = DispensaryReview::getDispensaryReviewByDispensaryReviewId($pdo, $id);
+					if($dispensaryReview !== null) {
+						$reply->data = $dispensaryReview;
+					}
+				} else if(empty($dispensaryReviewProfileId) === false) {
+					$dispensaryReviews = DispensaryReview::getDispensaryReviewByDispensaryReviewProfileId($pdo, $id);
+					if($dispensaryReviews !== null) {
+						$reply->data = $dispensaryReviews;
+					}
 
-			} else if(empty($dispensaryReviewProfileId) === false) {
-				$dispensaryReviews = DispensaryReview::getDispensaryReviewByDispensaryReviewProfileId($pdo, $id);
-				if($dispensaryReviews !== null) {
-					$reply->data = $dispensaryReviews;
+				} else if(empty($dispensaryReviewDispensaryId) === false) {
+					$dispensaryReviews = DispensaryReview::getDispensaryReviewByDispensaryReviewDispensaryId($pdo, $id);
+					if($dispensaryReviews !== null) {
+						$reply->data = $dispensaryReviews;
+					}
+				} else {
+					$dispensaryReviews = DispensaryReview::getAllDispensaryReviews($pdo);
+					if($dispensaryReviews !== null) {
+						$reply->data = $dispensaryReviews;
+					}
 				}
-
-			} else if(empty($dispensaryReviewDispensaryId) === false) {
-				$dispensaryReviews = DispensaryReview::getDispensaryReviewByDispensaryReviewDispensaryId($pdo, $id);
-				if($dispensaryReviews !== null) {
-					$reply->data = $dispensaryReviews;
-				}
-			} else {
-				$dispensaryReviews = DispensaryReview::getAllDispensaryReviews($pdo);
-				if($dispensaryReviews !== null) {
-					$reply->data = $dispensaryReviews;
-				}
-			}
 		} else if($method === "PUT" || $method === "POST") {
 
 			verifyXsrf();
@@ -85,3 +86,22 @@ try {
 			if(empty($requestObject->dispensaryReviewTxt) === true) {
 				throw(new \InvalidArgumentException ("No content for Review.", 405));
 			}
+
+		}
+
+	// update reply with exception information
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+
+// encode and return reply to front end caller
+echo json_encode($reply);
