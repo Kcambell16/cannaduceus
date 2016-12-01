7ce65b062c212b8ea030826444075c6bd985145b
Hector Lozano <hlozano2@cnm.edu>
2016-11-30 22:50:10 -0700

    added more data to restfulapi for strain review
    https://app.asana.com/0/201605659938138/220765076434886

--- public_html/api/strainReview/index.php
+++ public_html/api/strainReview/index.php
@@ -23,3 +23,19 @@ $reply->status = 200;
 $reply->data = null;
 // Here we create a new stdClass named $reply. A stdClass is basically an empty bucket that we can use to store things in.
 // We will use this object named $reply to store the results of the call to our API. The status 200 line adds a state variable to $reply called status and initializes it with the integer 200 (success code). The proceeding line adds a state variable to $reply called data. This is where the result of the API call will be stored. We will also update $reply->message as we proceed through the API.
+
+
+try {
+	//grab the mySQL DataBase connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cannaduceus.ini");
+
+
+	//determines which HTTP Method was used.
+	$method = array_key_exists("HTTP_x_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	$reply->method = $method;
+
+	//stores the Primary Key for the GET, DELETE, and PUT methods in $id. This key will come in the URL sent by the front end. If no key is present, $id will remain empty. Note that the input is filtered. Sanitize input.
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	$profileId = filter_input(INPUT_GET, "profileId", FILTER_VALIDATE_INT);
+	$strainId = filter_input(INPUT_GET, "strainId", FILTER_VALIDATE_INT);
+	$reviewTxt = filter_input(INPUT_GET, "reviewTxt", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
