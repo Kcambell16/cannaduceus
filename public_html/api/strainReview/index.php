93a33dcdd35ff7028393aa77cfb8620716173b33
Hector Lozano <hlozano2@cnm.edu>
2016-11-30 22:45:49 -0700

    added more data to restfulapi for strain review
    https://app.asana.com/0/201605659938138/220765076434886

--- public_html/api/strainReview/index.php
+++ public_html/api/strainReview/index.php
@@ -1,7 +1,25 @@
 <?php
+require_once dirname(__DIR__, 3 ) . "/php/classes/autoload.php";
+//require_once dirname(__DIR__, 3 ) . "/php/lib/xsrf.php";
+require_once "/etc/apache2/capstone-mysql/encrypted-config.php";
+
+use Edu\Cnm\Cannaduceus\StrainReview;
+
 /**
- * Created by PhpStorm.
- * User: hectorlozano
- * Date: 11/30/16
- * Time: 10:13 PM
- */
+ * * api for StrainReview class
+ *
+ * @author Hector Lozano <hlozano2@cnm.edu>
+ **/
+
+
+// Check the session status. If it is not active, start the session.
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+// prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+// Here we create a new stdClass named $reply. A stdClass is basically an empty bucket that we can use to store things in.
+// We will use this object named $reply to store the results of the call to our API. The status 200 line adds a state variable to $reply called status and initializes it with the integer 200 (success code). The proceeding line adds a state variable to $reply called data. This is where the result of the API call will be stored. We will also update $reply->message as we proceed through the API.
