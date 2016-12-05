25aaa4fd1b4e4de2456edf650fc3d49843c1ff71
Hector Lozano <hlozano2@cnm.edu>
2016-12-05 14:02:59 -0700

    started seed data for dispensary class

---
+++ sql/dispensaryseeddata.sql
@@ -0,0 +1,28 @@
+
+
+INSERT INTO DISPENSARY (dispensaryAttention, dispensaryCity, dispensaryEmail, dispensaryName, dispensaryPhone, dispensaryState, dispensaryStreet1, dispensaryStreet2, dispensaryUrl, dispensaryZipCode)
+	VALUES (null, 'Albuquerque', 'support@cannaceutics.org', 'Cannaceutics Inc', 505-672-8734, 'NM', 'P.O.Box 95705', null, 'www.cannaceutics.org', 87199);
+
+
+INSERT INTO DISPENSARY (dispensaryAttention, dispensaryCity, dispensaryEmail, dispensaryName, dispensaryPhone, dispensaryState, dispensaryStreet1, dispensaryStreet2, dispensaryUrl, dispensaryZipCode)
+VALUES (null, 'Albuquerque', 'info@cgcorrigan.org', 'CG Corrigan', 505-933-5599, 'NM', '6414 Gulton CT NE Suite A', null, 'www.cgcorrigan.org', 87109);
+
+
+INSERT INTO DISPENSARY (dispensaryAttention, dispensaryCity, dispensaryEmail, dispensaryName, dispensaryPhone, dispensaryState, dispensaryStreet1, dispensaryStreet2, dispensaryUrl, dispensaryZipCode)
+VALUES (null, 'Albuquerque', 'info.grrx@gmail.com', 'GrassRoots Rx', 505-835-5360, 'NM', '2717 2nd St NW', null, 'www.grrx.org', 87109);
+
+
+INSERT INTO DISPENSARY (dispensaryAttention, dispensaryCity, dispensaryEmail, dispensaryName, dispensaryPhone, dispensaryState, dispensaryStreet1, dispensaryStreet2, dispensaryUrl, dispensaryZipCode)
+	VALUES (null, 'Albuquerque', 'info@hesmmj.com', 'HES: Healthy Education Society', 505-312-7664, 'NM', '5401 Lomas Blvd. NE Suite A', null, 'www.hesmmj.com', 87111);
+
+
+INSERT INTO DISPENSARY (dispensaryAttention, dispensaryCity, dispensaryEmail, dispensaryName, dispensaryPhone, dispensaryState, dispensaryStreet1, dispensaryStreet2, dispensaryUrl, dispensaryZipCode)
+VALUES (null, 'Albuquerque', 'order@highdesertrelief.org', 'High Desert Relief', 505-750-4015, 'NM', '4730-C Pan American East Freeway', null, 'www.highdesertrelief.org', 87109);
+
+
+INSERT INTO DISPENSARY (dispensaryAttention, dispensaryCity, dispensaryEmail, dispensaryName, dispensaryPhone, dispensaryState, dispensaryStreet1, dispensaryStreet2, dispensaryUrl, dispensaryZipCode)
+VALUES (null, 'Albuquerque', 'contactus@medzen.info', 'MedZen Services Inc', 505-200-2367, 'NM', '4014 Central Ave NE', null, 'www.medzen.info', 87106);
+
+
+INSERT INTO DISPENSARY (dispensaryAttention, dispensaryCity, dispensaryEmail, dispensaryName, dispensaryPhone, dispensaryState, dispensaryStreet1, dispensaryStreet2, dispensaryUrl, dispensaryZipCode)
+VALUES (null, 'Albuquerque', 'info@minervacanna.com', 'Minerva Canna Group Inc', 505-888-2699, 'NM', '7103 4th st NW Building M', null, 'www.minervacanna.com', 87107);
