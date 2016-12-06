0dc7c94650d5cd3a6635a91b975ff5811ab2931e
Hector Lozano <hlozano2@cnm.edu>
2016-12-05 17:47:53 -0700

    added more seed data to dispensary class

--- sql/dispensaryseeddata.sql
+++ sql/dispensaryseeddata.sql
@@ -112,11 +112,12 @@ INSERT INTO DISPENSARY (dispensaryAttention, dispensaryCity, dispensaryEmail, di
 VALUES (null, 'Ruidoso', 'customerservice@compassionatedistributors.com', 'Compassionate Distributors, Inc', 575-258-1087, 'NM', '1216 Meachem Dr Ste 2', null, 'www.compassionatedistributors.com', 888350);
 
 
+INSERT INTO DISPENSARY (dispensaryAttention, dispensaryCity, dispensaryEmail, dispensaryName, dispensaryPhone, dispensaryState, dispensaryStreet1, dispensaryStreet2, dispensaryUrl, dispensaryZipCode)
+VALUES (null, 'Gallup', null, 'Red Barn Growers', 505-862-9707, 'NM', '204 South 3rd St', null, null, 87301);
 
 
-
-
-
+INSERT INTO DISPENSARY (dispensaryAttention, dispensaryCity, dispensaryEmail, dispensaryName, dispensaryPhone, dispensaryState, dispensaryStreet1, dispensaryStreet2, dispensaryUrl, dispensaryZipCode)
+VALUES (null, 'Hobbs', 'hobbs@ultrahealth.com', 'New Mexicann Natural Medicine, Inc', 575-964-1500, 'NM', '3211 N Dal Paso Ste B', null, 'www.newmexicann.org', 87533);
 
 
 
