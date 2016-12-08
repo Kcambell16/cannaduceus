6a6488c2c5ce36a55b2761e999010fa61ac9717d
Kelly Campbell <Kcampbell16@cnm.edu>
2016-12-07 19:34:55 -0700

    added google api https://app.asana.com/0/201605659938138/206915832495055

--- cannaduceus-ui/home.php
+++ cannaduceus-ui/home.php
@@ -109,6 +109,37 @@
 				</div>
 			</div>
 		</footer>
+		<!-- end of sticky footer -->
+
+		<!-- Google API -->
+		<div class="well well-lg">
+			<style>
+				#map {
+					height: 400px;
+					width: 100%;
+				}
+			</style>
+
+				<h3>Locate Dispensary</h3>
+				<div id="map"></div>
+				<script>
+					function initMap() {
+						var uluru = {lat: -25.363, lng: 131.044};
+						var map = new google.maps.Map(document.getElementById('map'), {
+							zoom: 4,
+							center: uluru
+						});
+						var marker = new google.maps.Marker({
+							position: uluru,
+							map: map
+						});
+					}
+				</script>
+				<script async defer
+						  src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
+				</script>
+		</div>
+		<!-- google api start -->
 
 
 
