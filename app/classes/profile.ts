2f04a816f287c21d11e8fa7d430904a08f25ed08
Nathan Sanchez <nsanchez121@cnm.edu>
2016-12-10 15:01:03 -0700

    its working pretty good https://app.asana.com/0/201605659938138/220765076434886

---
+++ app/classes/profile.ts
@@ -0,0 +1,3 @@
+export class Profile {
+	constructor(public profileId: number, public profileUserName: string, public profileEmail: string, public profileHash: string, public profileSalt: string, public profileActivation:string) {}
+}
