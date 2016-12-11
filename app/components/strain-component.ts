e825096c055e9eefc5f8cb002ae9594a2bf2381d
Nathan Sanchez <nsanchez121@cnm.edu>
2016-12-10 19:33:58 -0700

    woop

--- app/components/strain-component.ts
+++ app/components/strain-component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit, ViewChild} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
 import {StrainService} from "../services/strain-service";
+import {StrainReviewService} from "../services/strainreview-service";
 import {Strain} from "../classes/strain";
-import {Router} from "@angular/router";
 import {Status} from "../classes/status";
+import {StrainReview} from "../classes/strainReview";
 
 @Component({
 	templateUrl: "./templates/strain.php"
@@ -12,11 +14,14 @@ export class StrainComponent  implements OnInit {
 	@ViewChild("strainForm") strainForm : any;
 	strains: Strain[] = [];
 	strain: Strain = new Strain (null, "","","","","");
+	strainReviews: StrainReview[] = [];
+	strainReview: StrainReview = new StrainReview (null, "", "", null,"");
 	status: Status = null;
 
 	constructor(
 		private strainService: StrainService,
-		private router: Router
+		private strainReviewService: StrainReviewService,
+		private activatedRoute:ActivatedRoute
 	) {}
 
 	ngOnInit() : void {
@@ -24,9 +29,18 @@ export class StrainComponent  implements OnInit {
 	}
 
 	reloadStrains() : void {
-		this.strainService.getAllStrains()
-			.subscribe(strains => this.strains = strains);
+		this.activatedRoute.params
+			.switchMap((params : Params) => this.strainService.getStrainByStrainId(+params["strainId"]))
+			.subscribe(strains => {
+				this.strains = strains;
+
+				this.strainReviewService.getStrainReviewByStrainReviewStrainId(this.strain.strainId)
+					.subscribe(strainReviews => this.strainReviews = strainReviews);
+
+			});
+
 	}
+
 	// createStrain() : void {
 	// 	this.strainService.createStrain(this.strain)
 	// 		.subscribe(status => {
