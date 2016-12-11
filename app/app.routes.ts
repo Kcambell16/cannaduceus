bd5ed412f0cee43b392ed7d378aa989cf1c3061c
Kelly Campbell <Kcampbell16@cnm.edu>
2016-12-10 17:36:56 -0700

    api set for activation and signup and service and component created :heart_eyes_cat: https://app.asana.com/0/201605659938138/228579304637039

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -4,16 +4,19 @@ import {StrainComponent} from "./components/strain-component";
 import {DispensaryComponent} from "./components/dispensary-component";
 import {SignupComponent} from "./components/signup-component";
 import {SigninComponent} from "./components/signin-component";
+import {ActivationComponent} from "./components/activation-component";
 
 
-export const allAppComponents = [HomeComponent, StrainComponent, DispensaryComponent, SignupComponent, SigninComponent];
+export const allAppComponents = [HomeComponent, StrainComponent, DispensaryComponent, SignupComponent, SigninComponent, ActivationComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
 	{path: "strain", component: StrainComponent},
 	{path: "dispensary", component: DispensaryComponent},
 	{path: "singup", component: SignupComponent},
-	{path: "signin", component: SigninComponent}
+	{path: "signin", component: SigninComponent},
+	{path: "activation", component: ActivationComponent}
+	
 
 
 ];
