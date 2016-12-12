f42004e94c05e1982d635374f41bbcbf28e9c470
James Montoya <jamesmontoyaarts@gmail.com>
2016-12-12 08:31:16 -0700

    made medzen dispensary page

---
+++ cannaduceus-ui/ultraHealth.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
+
+		<!-- Custom CSS Goes HERE -->
+		<link rel="stylesheet" href="css/styles.css" type="text/css">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery - required for Bootstrap Components -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<title>Cannaduceus Dispensary page </title>
+
+	<body>
+
+		<header>
+			<!-- navbar --->
+			<nav class="navbar navbar-default navbar-inverse" >
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" routerLink="">Cannaduceus - Organtica</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a routerLink="">Home</a></li>
+							<li><a routerLink="/strain">Strain</a></li>
+							<li><a routerLink="/dispensary">Dispensary</a></li>
+							<li><a routerLink="/signup">Signin</a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+			<main>
+				<router-outlet></router-outlet>
+			</main>
+		</header>
+		<!-- Portfolio Item Heading -->
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Organtica
+					<small>Premiere Organic Cannabis</small>
+				</h1>
+			</div>
+		</div>
+		<!-- /.row -->
+
+		<!-- Portfolio Item Row -->
+		<div class="row">
+
+			<div class="col-md-8">
+				<img class="img-responsive" src="images/ultraHealth.png" alt="Ultra Health">
+			</div>
+
+			<div class="col-md-4">
+				<h3>Ultra Health</h3>
+				<p>As one of the new dispensaries licensed by the state in 2015 we are the new kid on the block but our experience goes well farther.  We carry some of New Mexico's best premium organic cannabis products.</p> </h3>
+				<ul>
+					<li>Lorem Ipsum</li>
+					<li>Dolor Sit Amet</li>
+					<li>Consectetur</li>
+					<li>Adipiscing Elit</li>
+				</ul>
+			</div>
+
+		</div>
+		<!-- /.row -->
+
+		<!-- Related Projects Row -->
+		<div class="row">
+
+			<div class="col-lg-12">
+				<h3 class="page-header">Ultra Health</h3>
+			</div>
+
+			<div class="col-sm-3 col-xs-6">
+				<a href="#">
+					<img class="img-responsive portfolio-item" src="images/organtica1.gif" alt="">
+				</a>
+			</div>
+
+			<div class="col-sm-3 col-xs-6">
+				<a href="#">
+					<img class="img-responsive portfolio-item" src="images/platinum%20rider.gif" alt="">
+				</a>
+			</div>
+
+			<div class="col-sm-3 col-xs-6">
+				<a href="#">
+					<img class="img-responsive portfolio-item" src="images/Hawaii%205-0.gif" alt="">
+				</a>
+			</div>
+
+			<div class="col-sm-3 col-xs-6">
+				<a href="#">
+					<img class="img-responsive portfolio-item" src="images/headband.gif" alt="">
+				</a>
+			</div>
+
+		</div>
+		<!-- /.row -->
+
+		<!-- strain info model ends here -->
+
+		<!-- footer inverse --->
+		<footer class="footer navbar-inverse">
+			<div class="container">
+				<div class="row">
+					<!-- contact section in footer-->
+					<div class="col-xs-12 col-md-6">
+						<a src="/cannaduceus-ui/images/cannaduceus-logo.png" alt="logo"></a>
+						<h4> Contact Us</h4>
+						<p>Email: Admin@Cannaduceus.com</p>
+						<p>Phone: 1(800)555-5555</p>
+						<p href="url...">Privacy Policy</p>
+						<p href="rul...."> Terms of Use</p>
+					</div>
+					<div class="col-xs-12 col-md-6">
+						<strong>Company</strong>
+						<p>About Us</p>
+						<p>Careers</p>
+						<h4>Connect with Cannaduceus</h4>
+						<a class="btn btn-social-icon btn-twitter">
+							<span class="fa fa-twitter"> Twitter</span>
+						</a>
+						<a class="btn btn-social-icon btn-facebook">
+							<span class="fa fa-facebook"> Facebook</span>
+						</a>
+						<a class="btn btn-social-icon btn-instagram">
+							<span class="fa fa-instagram"> Instagram</span>
+						</a>
+						<!-- for additional info if needed-->
+					</div>
+					<div class="col-xs-6 col-sm-4">
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- end of sticky footer -->
+
+
+
+	</body>
+</html>
--- public_html/api/signUp/index.php
+++ public_html/api/signUp/index.php
@@ -26,41 +26,31 @@ try {
 	if($method === "POST") {
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
-		if(empty($requestObject->profileFirstName) === true) {
-			throw(new \InvalidArgumentException ("Must fill in first name", 405));
+		if(empty($requestObject->profileUserName) === true) {
+			throw(new \InvalidArgumentException ("Must fill in profile name", 405));
 		} else {
-			$profileFirstName = filter_var($requestObject->profileFirstName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-		}
-		if(empty($requestObject->profileLastName) === true) {
-			throw(new \InvalidArgumentException ("Must fill in valid last name", 405));
-		} else {
-			$profileLastName = filter_var($requestObject->profileLastName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-		}
-		if(empty($requestObject->profileProfileName) === true) {
-			throw(new \InvalidArgumentException ("Must input valid profile name", 405));
-		} else {
-			$profileProfileName = filter_var($requestObject->profileProfileName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-		}
-		if(empty($requestObject->password) === true) {
-			throw(new \InvalidArgumentException ("Must input valid password", 405));
-		} else {
-			$password = filter_var($requestObject->password, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+			$profileUserName = filter_var($requestObject->profileUserName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		}
 		if(empty($requestObject->profileEmail) === true) {
 			throw(new \InvalidArgumentException ("Must enter a valid email address", 405));
 		} else {
 			$profileEmail = filter_var($requestObject->profileEmail, FILTER_SANITIZE_EMAIL);
 		}
+		if(empty($requestObject->password) === true) {
+			throw(new \InvalidArgumentException ("Must input valid password", 405));
+		} else {
+			$password = filter_var($requestObject->password, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		}
+
 		$salt = bin2hex(random_bytes(32));
 		$hash = hash_pbkdf2("sha512", $password, $salt, 262144);
 		$profileAccessLevel = 0;
 		$profileActivationToken = bin2hex(random_bytes(16));
-		$profile = new Cannaduceus\Profile(null, null, $profileAccessLevel, $profileActivationToken, $profileEmail,
-			$profileFirstName, $hash, $profileLastName, $salt, $profileProfileName);
+		$profile = new Cannaduceus\Profile(null, $profileUserName, $profileEmail,$hash, $salt, $profileActivation);
 		$profile->insert($pdo);
 		$messageSubject = "ABQ Brew Crew Welcomes You! -- Account Activation";
 		//building the activation link that can travel to another server and still work. This is the link that will be clicked to confirm the account.
-		// FIXME: make sure URL is /public_html/php/apis/activation/$activation
+		// FIXME: make sure URL is /public_html/php/api/activation/$activation
 		$basePath = dirname($_SERVER["SCRIPT_NAME"], 2);
 		$urlGlue = $basePath . "/activation/" . $profileActivationToken;
 		$confirmLink = "https://" . $_SERVER["SERVER_NAME"] . $urlGlue;
@@ -69,7 +59,7 @@ try {
 <p>In order to start rating your favorite local dispensaries please visit the following URL to set a new password and complete the registration process: </p>
 <p><a href="$confirmLink">$confirmLink</a></p>
 EOF;
-		$response = sendEmail($profileEmail, $profileFirstName, $profileLastName, $messageSubject, $message);
+		$response = sendEmail($profileUserName, $profileEmail, $messageSubject, $message);
 		if($response === "Email sent.") {
 			$reply->message = "Sign up was successful, please check your email for activation message.";
 		} else {
