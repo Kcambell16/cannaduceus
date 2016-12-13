e7e80f2f263356bd98cd59995fc01b2331dd665d
James Montoya <jamesmontoyaarts@gmail.com>
2016-12-13 15:21:45 -0700

    created sign in and sign up component

--- public_html/templates/signup.php
+++ public_html/templates/signup.php
@@ -1,22 +1,17 @@
 <!-- -->
 <div class="container">
 	<div class="row centered-form">
-		<div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+		<div class="col-xs-12 col-sm-8 col-md-10 col-sm-offset-2 col-md-offset-1">
 			<div class="panel panel-default">
 				<div class="panel-heading">
-					<h3 class="panel-title">Please sign up for Bootsnipp <small>It's free!</small></h3>
+					<h3 class="panel-title">Sign Up With Cannaduceus<small>It's free!</small></h3>
 				</div>
 				<div class="panel-body">
 					<form role="form">
 						<div class="row">
-							<div class="col-xs-6 col-sm-6 col-md-6">
+							<div class="col-xs-12 col-sm-12 col-md-12">
 								<div class="form-group">
 									<input type="text" name="first_name" id="first_name" class="form-control input-sm" placeholder="First Name">
-								</div>
-							</div>
-							<div class="col-xs-6 col-sm-6 col-md-6">
-								<div class="form-group">
-									<input type="text" name="last_name" id="last_name" class="form-control input-sm" placeholder="Last Name">
 								</div>
 							</div>
 						</div>
