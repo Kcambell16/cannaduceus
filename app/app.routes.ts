408093e9afdb2fbe5ff14c89de622da232eca467
Nathan Sanchez <nsanchez121@cnm.edu>
2016-12-13 15:16:08 -0700

    destroyed all lawn nomes from Nome, AK https://app.asana.com/0/201605659938138/228579304637039

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,16 +2,16 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
 import {StrainComponent} from "./components/strain-component";
 import {DispensaryComponent} from "./components/dispensary-component";
-import {SignupComponent} from "./components/signup-component";
+import {SignUpComponent} from "./components/signup-component";
 import {SigninComponent} from "./components/signin-component";
 
-export const allAppComponents = [HomeComponent, StrainComponent, DispensaryComponent, SignupComponent, SigninComponent];
+export const allAppComponents = [HomeComponent, StrainComponent, DispensaryComponent, SignUpComponent, SigninComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
 	{path: "strain", component: StrainComponent},
 	{path: "dispensary", component: DispensaryComponent},
-	{path: "signup", component: SignupComponent},
+	{path: "signup", component: SignUpComponent},
 	{path: "signin", component: SigninComponent},
 
 ];
