6f9c624a719d4049b4a29a0a1f39ce3ef203cc2f
Kelly Campbell <Kcampbell16@cnm.edu>
2016-12-14 11:30:08 -0700

    added amnesia template and also module

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -5,8 +5,9 @@ import {DispensaryComponent} from "./components/dispensary-component";
 import {SignUpComponent} from "./components/signup-component";
 import {SigninComponent} from "./components/signin-component";
 import {StrainDetailComponent} from "./components/straindetail-component";
+import {AmnesiaComponent} from "./components/amnesia-component";
 
-export const allAppComponents = [HomeComponent, StrainComponent, DispensaryComponent, SignUpComponent, SigninComponent, StrainDetailComponent];
+export const allAppComponents = [HomeComponent, StrainComponent, DispensaryComponent, SignUpComponent, SigninComponent, StrainDetailComponent, AmnesiaComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
@@ -15,6 +16,7 @@ export const routes: Routes = [
 	{path: "signup", component: SignUpComponent},
 	{path: "signin", component: SigninComponent},
 	{path: "straindetail", component: StrainDetailComponent},
+	{path: "amnesia", component: AmnesiaComponent},
 
 ];
 
