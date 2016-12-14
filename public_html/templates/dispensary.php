b861b0740493d2a3d717ff6d5a6f3ab41f5e5909
James Montoya <jamesmontoyaarts@gmail.com>
2016-12-14 16:15:07 -0700

    working on the dispensary page added pictures

--- public_html/templates/dispensary.php
+++ public_html/templates/dispensary.php
@@ -19,7 +19,7 @@
 
 		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
 			<div class="thumbnail">
-				<img src="app/images/dispensaries/organtica1.gif" alt="organtica">
+				<img src="../app/images/dispensaries/organticaMain.png" alt="organtica">
 				<div class="caption">
 					<h3>Organtica</h3>
 					<h4>Rating</h4>
@@ -30,7 +30,7 @@
 
 		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
 			<div class="thumbnail">
-				<img src="/app/images/dispensaries/ultraHealth.png" alt="Ultra Health">
+				<img src="../app/images/dispensaries/ultraHealth.png" alt="Ultra Health">
 				<div class="caption">
 					<h3>Ultrahealth</h3>
 					<h4>Rating</h4>
@@ -41,7 +41,7 @@
 
 		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
 			<div class="thumbnail">
-				<img src="app/images/dispensaries/medzen_sign.jpg" alt="Med Zen">
+				<img src="../app/images/dispensaries/medzen_sign.jpg" alt="Med Zen">
 				<div class="caption">
 					<h3>Med Zen</h3>
 					<h4>Rating</h4>
@@ -52,7 +52,7 @@
 
 		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
 			<div class="thumbnail">
-				<img src="app/images/dispensaries/newMexicann.png" alt="New Mexicann">
+				<img src="../app/images/dispensaries/newMexicann.png" alt="New Mexicann">
 				<div class="caption">
 					<h3>New Mexicann</h3>
 					<h4>Rating</h4>
@@ -67,7 +67,7 @@
 
 		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
 			<div class="thumbnail">
-				<img src="app/images/dispensaries/rGreenleaf.png" alt="R. Greenleaf">
+				<img src="../app/images/dispensaries/rGreenleaf.png" alt="R. Greenleaf">
 				<div class="caption">
 					<h3>R. Greenleaf</h3>
 					<h4>Rating</h4>
@@ -78,83 +78,38 @@
 
 		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
 			<div class="thumbnail">
-				<img src="app/images/dispensaries/verdes.png" alt="Verdes">
+				<img src="../app/images/dispensaries/verdes.png" alt="Verdes">
 				<div class="caption">
-					<h3>Bubba Kush</h3>
+					<h3>Verdes</h3>
 					<h4>Rating</h4>
-					<a href="#" class="btn btn-default text-center">More Info</a>
+					<a href="#" class="btn btn-default text-center">Verdes</a>
 				</div>
 			</div>
 		</div>
 
 		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
 			<div class="thumbnail">
-				<img src="images/buddha-tahoe.jpeg" alt="buddha-tahoe">
+				<img src="../app/images/dispensaries/sandiaBotanicals.png" alt="Sandia Botanicals">
 				<div class="caption">
-					<h3>Buddha Tahoe</h3>
+					<h3>Sandia Botanicals</h3>
 					<h4>Rating</h4>
-					<a href="#" class="btn btn-default text-center">More Info</a>
+					<a href="#" class="btn btn-default text-center">Sandia Botanicals</a>
 				</div>
 			</div>
 		</div>
 
 		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
 			<div class="thumbnail">
-				<img src="images/cannatonic.jpeg" alt="cannatonic">
+				<img src="../app/images/dispensaries/sevenClover.jpg" alt="7 clover">
 				<div class="caption">
-					<h3>Cannatonic</h3>
+					<h3>Seven Clover</h3>
 					<h4>Rating</h4>
-					<a href="#" class="btn btn-default text-center">More Info</a>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div class="row text-center">
-
-		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
-			<div class="thumbnail">
-				<img src="images/catarac-kush.jpeg" alt="catarac-kush">
-				<div class="caption">
-					<h3>Catarac Kush</h3>
-					<h4>Rating</h4>
-					<a href="#" class="btn btn-default text-center">More Info</a>
+					<a href="#" class="btn btn-default text-center">Seven Clover</a>
 				</div>
 			</div>
 		</div>
 
-		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
-			<div class="thumbnail">
-				<img src="images/chocolope.jpeg" alt="chocolope">
-				<div class="caption">
-					<h3>Chocolope</h3>
-					<h4>Rating</h4>
-					<a href="#" class="btn btn-default text-center">More Info</a>
-				</div>
-			</div>
-		</div>
 
-		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
-			<div class="thumbnail">
-				<img src="images/critical-super-silver-haze.jpeg" alt="critical-super-silver-haze">
-				<div class="caption">
-					<h3>Critical Super Silver Haze</h3>
-					<h4>Rating</h4>
-					<a href="#" class="btn btn-default text-center">More Info</a>
-				</div>
-			</div>
-		</div>
-
-		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
-			<div class="thumbnail">
-				<img src="images/dark-shadow-haze.jpeg" alt="dark-shadow-haze">
-				<div class="caption">
-					<h3>Dark Shadow Haze</h3>
-					<h4>Rating</h4>
-					<a href="#" class="btn btn-default text-center">More Info</a>
-				</div>
-			</div>
-		</div>
 	</div>
 </div>
 <!-- Page Features -->
