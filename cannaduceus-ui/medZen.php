f53c30dc08388d47c0cbcdd3a607701d8a026a3b
James Montoya <jamesmontoyaarts@gmail.com>
2016-12-14 01:21:10 -0700

    working on static pages for demo

--- cannaduceus-ui/medZen.php
+++ cannaduceus-ui/medZen.php
@@ -130,16 +130,18 @@
 
 			<div>
 			<h1>Current Menu</h1>
-					<div id="grassMenu">
+
 						<div class="row">
 						<div class="col-xs-10 col-sm-10 col-md-6 col-lg-6">
-							<img class="img-responsive" src="/app/images/grassMenu.png">
+							<img class="img-responsive" src="../app/images/grassMenu.png">
 						</div>
 						</div>
-					</div>
+
 			</div>
 
+			<div id="googleMap">
 			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13060.444707699147!2d-106.5992402!3d35.0789548!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4d976774ee601ff7!2sMedzen+Services!5e0!3m2!1sen!2sus!4v1481695831651" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
 
 		</div>
 		<!-- /.row -->
