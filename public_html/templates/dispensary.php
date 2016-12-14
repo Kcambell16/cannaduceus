4ae2eb65096c31c36e566fded1e60aa4e38aa528
James Montoya <jamesmontoyaarts@gmail.com>
2016-12-14 11:24:23 -0700

    copy pasted strain view html into dispensary view

--- public_html/templates/dispensary.php
+++ public_html/templates/dispensary.php
@@ -1,1 +1,160 @@
 <h3>this is the dispensary view</h3>
+
+<div class="jumbotron text-center">
+	<h1>Find Your Perfect Strain</h1>
+	<p class="lead"> Explore strains and their affects</p>
+</div>
+
+<!-- review section -->
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+			<h3>Dispensaries</h3>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row text-center">
+
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
+			<div class="thumbnail">
+				<img src="images/alaskan-thunder-funk.jpeg" alt="bubba-kush">
+				<div class="caption">
+					<h3>Alaskan Thunder Funk</h3>
+					<h4>Rating</h4>
+					<a href="#" class="btn btn-default text-center">More Info</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
+			<div class="thumbnail">
+				<img src="images/amnesia.jpeg" alt="amnesia">
+				<div class="caption">
+					<h3>Amnesia</h3>
+					<h4>Rating</h4>
+					<a href="#" class="btn btn-default text-center">More Info</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
+			<div class="thumbnail">
+				<img src="images/bananasplit.jpeg" alt="bananasplit">
+				<div class="caption">
+					<h3>Banana Split</h3>
+					<h4>Rating</h4>
+					<a href="#" class="btn btn-default text-center">More Info</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
+			<div class="thumbnail">
+				<img src="images/berrykush.jpeg" alt="berrykush">
+				<div class="caption">
+					<h3>Berry Kush</h3>
+					<h4>Rating</h4>
+					<a href="#" class="btn btn-default text-center">More Info</a>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="row text-center">
+
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
+			<div class="thumbnail">
+				<img src="images/bewitched.jpeg" alt="bewitched">
+				<div class="caption">
+					<h3>Bewitched</h3>
+					<h4>Rating</h4>
+					<a href="#" class="btn btn-default text-center">More Info</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
+			<div class="thumbnail">
+				<img src="images/bubbakush.jpeg" alt="bubbakush">
+				<div class="caption">
+					<h3>Bubba Kush</h3>
+					<h4>Rating</h4>
+					<a href="#" class="btn btn-default text-center">More Info</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
+			<div class="thumbnail">
+				<img src="images/buddha-tahoe.jpeg" alt="buddha-tahoe">
+				<div class="caption">
+					<h3>Buddha Tahoe</h3>
+					<h4>Rating</h4>
+					<a href="#" class="btn btn-default text-center">More Info</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
+			<div class="thumbnail">
+				<img src="images/cannatonic.jpeg" alt="cannatonic">
+				<div class="caption">
+					<h3>Cannatonic</h3>
+					<h4>Rating</h4>
+					<a href="#" class="btn btn-default text-center">More Info</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row text-center">
+
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
+			<div class="thumbnail">
+				<img src="images/catarac-kush.jpeg" alt="catarac-kush">
+				<div class="caption">
+					<h3>Catarac Kush</h3>
+					<h4>Rating</h4>
+					<a href="#" class="btn btn-default text-center">More Info</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
+			<div class="thumbnail">
+				<img src="images/chocolope.jpeg" alt="chocolope">
+				<div class="caption">
+					<h3>Chocolope</h3>
+					<h4>Rating</h4>
+					<a href="#" class="btn btn-default text-center">More Info</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
+			<div class="thumbnail">
+				<img src="images/critical-super-silver-haze.jpeg" alt="critical-super-silver-haze">
+				<div class="caption">
+					<h3>Critical Super Silver Haze</h3>
+					<h4>Rating</h4>
+					<a href="#" class="btn btn-default text-center">More Info</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 hero-feature">
+			<div class="thumbnail">
+				<img src="images/dark-shadow-haze.jpeg" alt="dark-shadow-haze">
+				<div class="caption">
+					<h3>Dark Shadow Haze</h3>
+					<h4>Rating</h4>
+					<a href="#" class="btn btn-default text-center">More Info</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Page Features -->
